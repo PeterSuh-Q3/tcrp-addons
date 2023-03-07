4f5f7891a1054cafb0420bf26b1da674418a724b
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-03-07 15:41:36 +0900

    Add files via upload

--- 9p/src/install.sh
+++ 9p/src/install.sh
@@ -15,6 +15,7 @@ echo "link depmod,modprobe to kmod"
 [ ! -f /usr/sbin/depmod ] && ln -s /bin/kmod /usr/sbin/depmod
 [ ! -f /usr/sbin/modprobe ] && ln -s /bin/kmod /usr/sbin/modprobe
 tar xvfz /exts/tcrp-9p/${TARGET_PLATFORM}-${LINUX_VER}.tgz -C /
+mv -f /root/usr/lib/modules/9p*.ko /usr/lib/modules
 echo "Loading 9p module"
 /usr/sbin/depmod -a
 /usr/sbin/modprobe 9p
