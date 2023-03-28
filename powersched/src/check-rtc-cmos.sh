49a59cf5bc21e465793fca39f7444ef4e7cd724d
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-03-28 19:37:15 +0900

    Update check-rtc-cmos.sh

--- powersched/src/check-rtc-cmos.sh
+++ powersched/src/check-rtc-cmos.sh
@@ -4,7 +4,7 @@
 
 echo -n "Loading module rtc-cmos -> "
 
-if [ `/sbin/lsmod |grep -i rtc-cmos|wc -l` -gt 0 ] ; then
+if [ `/sbin/lsmod |grep -i rtc-cmos|wc -l` -gt 0 ]; then
         echo "Module rtc-cmos loaded succesfully"
         else echo "Module rtc-cmos is not loaded "
 fi
