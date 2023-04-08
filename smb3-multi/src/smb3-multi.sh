8cd2b25a4a9639b8b98ade81b2f722e5e62540eb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-04-08 11:30:18 +0900

    Add files via upload

--- smb3-multi/src/smb3-multi.sh
+++ smb3-multi/src/smb3-multi.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-
 echo "Add the smb3 multichannel configuration to the /etc/samba/smb.conf file."
 [ $(cat /etc/samba/smb.conf | grep "server multi channel support=yes" | wc -l) -eq 0 ] && echo "server multi channel support=yes" >> /etc/samba/smb.conf
 [ $(cat /etc/samba/smb.conf | grep "aio read size=1" | wc -l) -eq 0 ] && echo "aio read size=1" >> /etc/samba/smb.conf
 [ $(cat /etc/samba/smb.conf | grep "aio write size=1" | wc -l) -eq 0 ] && echo "aio write size=1" >> /etc/samba/smb.conf
-
 cat /etc/samba/smb.conf | grep -e multi -e aio
