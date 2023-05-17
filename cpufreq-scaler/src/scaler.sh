fc8c408ec729627daa8fbcc2db6ac2f18abd49d8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-05-18 00:09:21 +0900

    Fix bug again (synology doesn't have lscpu)

--- cpufreq-scaler/src/scaler.sh
+++ cpufreq-scaler/src/scaler.sh
@@ -20,7 +20,8 @@ set -euo pipefail
 function main {
 
   # Get cpu cores count minus 1, to allow maping from 0
-  cpucorecount=$(lscpu |grep CPU\(s\): | awk '{print $2 - 1 }')
+  cpucorecount=$(cat /proc/cpuinfo | grep processor | wc -l)
+  cpucorecount=$((cpucorecount - 1))
   governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 
   # Rereive allowed cpu freq on the system
@@ -77,7 +78,8 @@ function main {
 while true; do
 
   # Get cpu cores count minus 1, to allow maping from 0
-  cpucorecount=$(lscpu |grep CPU\(s\): | awk '{print $2 - 1 }')
+  cpucorecount=$(cat /proc/cpuinfo | grep processor | wc -l)
+  cpucorecount=$((cpucorecount - 1))
   governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 
   # Set correct cpufreq governor to allow user defined frequency scaling
