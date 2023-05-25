7625a898a16e67dece84df7e0835fb3e6b7009be
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-05-25 13:20:50 +0900

    Create installondsm.sh

---
+++ reboottotcrp/installondsm.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Manual execution scheduler registration for tcrp-reboot.sh"
+curl -kL https://github.com/PeterSuh-Q3/tcrp-addons/raw/main/reboottotcrp/src/tcrp-reboot.sh -o /usr/sbin/tcrp-reboot.sh
+chmod 755 /usr/sbin/tcrp-reboot.sh
+if [ -f /usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
+    echo "insert RebootToTcrp task"
+    /bin/sqlite3 /usr/syno/etc/esynoscheduler/esynoscheduler.db <<EOF
+INSERT INTO task VALUES('RebootToTcrp', '', '-', '', 0, 0, 0, 0, '', 0, '/usr/sbin/tcrp-reboot.sh "config"', 'script', '{}', '', '', '{}', '{}');
+EOF
+else
+    echo "copy RebootToTcrp task db"
+    mkdir -p /usr/syno/etc/esynoscheduler
+    curl -kL https://github.com/PeterSuh-Q3/tcrp-addons/raw/main/reboottotcrp/src/esynoscheduler.db -o /usr/syno/etc/esynoscheduler/esynoscheduler.db
+fi
