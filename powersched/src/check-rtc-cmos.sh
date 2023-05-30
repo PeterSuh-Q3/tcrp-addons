6aa81ca2a10611b0401b4c62f3e996e5528cd03c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-05-30 21:24:49 +0900

    Add files via upload

--- powersched/src/check-rtc-cmos.sh
+++ powersched/src/check-rtc-cmos.sh
@@ -2,7 +2,9 @@
 # Checking modules is loaded
 #
 
-echo -n "Loading module rtc_cmos -> "
+insmod /lib/modules/rtc-cmos.ko
+
+echo -n "Check Loading module rtc_cmos -> "
 
 if [ `/sbin/lsmod |grep -i rtc_cmos|wc -l` -gt 0 ]; then
         echo "Module rtc_cmos loaded succesfully"
