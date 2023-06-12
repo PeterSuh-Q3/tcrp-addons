5fb49d29650084f5e1392ca10f36b305ec8b5987
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-12 12:43:26 +0900

    Update installondsm.sh

--- nvme-cache/installondsm.sh
+++ nvme-cache/installondsm.sh
@@ -1,8 +1,8 @@
-
-echo "Installing NVMe cache enabler tools"
-cp -vf nvme-cache.sh /tmpRoot/usr/sbin/nvme-cache.sh
-chmod 755 /tmpRoot/usr/sbin/nvme-cache.sh
-cat > /tmpRoot/etc/systemd/system/nvme-cache.service <<'EOF'
+#!/bin/sh
+echo "Installing NVMe cache enabler service"
+cp -vf nvme-cache.sh /usr/sbin/nvme-cache.sh
+chmod 755 /usr/sbin/nvme-cache.sh
+cat > /etc/systemd/system/nvme-cache.service <<'EOF'
 [Unit]
 Description=NVMe cache enabler schedule
 [Service]
@@ -11,5 +11,5 @@ ExecStart=/usr/sbin/nvme-cache.sh
 [Install]
 WantedBy=multi-user.target
 EOF
-mkdir -p /tmpRoot/etc/systemd/system/multi-user.target.wants
-ln -sf /etc/systemd/system/nvme-cache.service /tmpRoot/etc/systemd/system/multi-user.target.wants/nvme-cache.service
+mkdir -p /etc/systemd/system/multi-user.target.wants
+ln -sf /etc/systemd/system/nvme-cache.service /etc/systemd/system/multi-user.target.wants/nvme-cache.service
