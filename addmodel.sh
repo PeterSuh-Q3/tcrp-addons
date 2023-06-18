964be03ff36e466fc8a3c812d57a36056beb2aa8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-18 17:23:43 +0900

    Update addmodel.sh

--- addmodel.sh
+++ addmodel.sh
@@ -9,9 +9,11 @@ model="$1"
 baseurl="https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master"  # Base URL
 
 ls -d */ | grep -v -e "9p" -e "disks" | while IFS= read -r dir; do
-  echo "Adding model $model for ${dir} rpext-index.json"
-  jsonfile="./${dir}/rpext-index.json"
+  echo "Adding model $model for ${dir}rpext-index.json"
+  jsonfile="./${dir}rpext-index.json"
 
-  model_url="${baseurl}/${dir}/recipes/universal.json"
-  jq --arg model "$model" --arg url "$model_url" '.releases += { ($model): $url }' "$jsonfile" > temp.json && mv temp.json "$jsonfile"
+  model_url="${baseurl}/${dir}recipes/universal.json"
+  jq --arg model "${model}_42218" --arg url "$model_url" '.releases += { ($model): $url }' "$jsonfile" > temp.json && mv temp.json "$jsonfile"
+  jq --arg model "${model}_42962" --arg url "$model_url" '.releases += { ($model): $url }' "$jsonfile" > temp.json && mv temp.json "$jsonfile"
+  jq --arg model "${model}_64570" --arg url "$model_url" '.releases += { ($model): $url }' "$jsonfile" > temp.json && mv temp.json "$jsonfile"  
 done
