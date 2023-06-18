109d53b36d66282b9e8aebb54f2b4289c6d3d985
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-18 16:54:04 +0900

    Update addmodel.sh

--- addmodel.sh
+++ addmodel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ls -d */ | grep -v -e "9p" -e "disks"
-for D in `ls -d */ | grep -v -e "9p" -e "disks"`; do
-  E=$(echo ${D} | sed 's#/##')
-  echo "Add model ${1} for ${E} rpext-index.json"  
-  jsonfile=$(jq '.releases |= .+ {$1_42218: "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json"}' "./${E}/rpext-index.json") && echo $jsonfile | jq . > "./${E}/rpext-index.json"
-  jsonfile=$(jq '.releases |= .+ {$1_42962: "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json"}' "./${E}/rpext-index.json") && echo $jsonfile | jq . > "./${E}/rpext-index.json"
-  jsonfile=$(jq '.releases |= .+ {$1_64570: "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json"}' "./${E}/rpext-index.json") && echo $jsonfile | jq . > "./${E}/rpext-index.json"  
+for D in $(ls -d */ | grep -v -e "9p" -e "disks"); do
+  E=$(echo "${D}" | sed 's#/##')
+  echo "Add model ${1} for ${E} rpext-index.json"
+  jsonfile=$(jq --arg model "${1}" --arg url "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json" '.releases += { ($model_42218): $url }' "./${E}/rpext-index.json") && echo "$jsonfile" > "./${E}/rpext-index.json"
+  jsonfile=$(jq --arg model "${1}" --arg url "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json" '.releases += { ($model_42662): $url }' "./${E}/rpext-index.json") && echo "$jsonfile" > "./${E}/rpext-index.json"
+  jsonfile=$(jq --arg model "${1}" --arg url "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json" '.releases += { ($model_64570): $url }' "./${E}/rpext-index.json") && echo "$jsonfile" > "./${E}/rpext-index.json"  
 done
