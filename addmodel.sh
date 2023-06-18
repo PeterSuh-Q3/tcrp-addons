bd83e301cc170950e12f768f47dd98d1c22daab6
PeterSuh-Q3 <dante9000@gmail.com>
2023-06-18 14:50:37 +0900

    Fix

--- addmodel.sh
+++ addmodel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ls -d *-*/ | grep -v -e "9p" -e "disks"
-for D in `ls -d *-*/ | grep -v -e "9p" -e "disks"`; do
+ls -d */ | grep -v -e "9p" -e "disks"
+for D in `ls -d */ | grep -v -e "9p" -e "disks"`; do
   E=$(echo ${D} | sed 's#/##')
   echo "Add model for ${E} directory"  
 done
