4bec54a73ad033f8b58871567549b5f4e8b36292
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-18 16:47:16 +0900

    Update addmodel.sh

--- addmodel.sh
+++ addmodel.sh
@@ -3,5 +3,8 @@
 ls -d */ | grep -v -e "9p" -e "disks"
 for D in `ls -d */ | grep -v -e "9p" -e "disks"`; do
   E=$(echo ${D} | sed 's#/##')
-  echo "Add model for ${E} directory"  
+  echo "Add model ${1} for ${E} rpext-index.json"  
+  jsonfile=$(jq '.releases |= .+ {$1_42218: "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json"}' "./${E}/rpext-index.json") && echo $jsonfile | jq . > "./${E}/rpext-index.json"
+  jsonfile=$(jq '.releases |= .+ {$1_42962: "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json"}' "./${E}/rpext-index.json") && echo $jsonfile | jq . > "./${E}/rpext-index.json"
+  jsonfile=$(jq '.releases |= .+ {$1_64570: "https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master/${E}/recipes/universal.json"}' "./${E}/rpext-index.json") && echo $jsonfile | jq . > "./${E}/rpext-index.json"  
 done
