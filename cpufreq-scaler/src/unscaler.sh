b61c3aae26f7846697c039a80c29522413e4516b
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-21 12:59:11 +0900

    Create unscaler.sh

---
+++ cpufreq-scaler/src/unscaler.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Make things safer
+set -euo pipefail
+
+# Get cpu cores count minus 1, to allow maping from 0
+cpucorecount=$(cat /proc/cpuinfo | grep processor | wc -l)
+cpucorecount=$((cpucorecount - 1))
+governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+
+# Set correct cpufreq governor to allow user defined frequency scaling
+if [ "$governor" != "performance" ]; then
+  for i in $(seq 0 "${cpucorecount}"); do
+    echo "performance" >/sys/devices/system/cpu/cpu"${i}"/cpufreq/scaling_governor
+  done
+fi
