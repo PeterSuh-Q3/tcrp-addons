d9b0879ce9fb04569d9512ec2dc2e84903a244e6
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-30 00:31:33 +0900

    Add files via upload

--- nvme-cache/src/install-nvme-cache.sh
+++ nvme-cache/src/install-nvme-cache.sh
@@ -119,21 +119,7 @@ elif [ "$HASBOOTED" = "yes" ]; then
     cp -vf /etc/extensionPorts /tmpRoot/etc.defaults/
   fi
 
+  cat /etc/synoinfo.conf | grep -e support_m2_pool -e supportnvme
   cp -vf /etc/synoinfo.conf /tmpRoot/etc/
   cp -vf /etc/synoinfo.conf /tmpRoot/etc.defaults/
-
-  cp -vf nvme-cache.sh /tmpRoot/usr/sbin/
-  chmod 755 /tmpRoot/usr/sbin/nvme-cache.sh
-  
-cat > /tmpRoot/etc/systemd/system/nvme-cache.service <<'EOF'
-[Unit]
-Description=NVMe cache enabler schedule
-[Service]
-Type=oneshot
-ExecStart=/usr/sbin/nvme-cache.sh
-[Install]
-WantedBy=multi-user.target
-EOF
-  mkdir -p /tmpRoot/etc/systemd/system/multi-user.target.wants
-  ln -sf /etc/systemd/system/nvme-cache.service /tmpRoot/etc/systemd/system/multi-user.target.wants/nvme-cache.service  
 fi
