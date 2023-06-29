50f2451d2af5297d1f95324ea6f0d80d51b60a75
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-29 22:17:21 +0900

    Add files via upload

--- nvme-cache/src/install-nvme-cache.sh
+++ nvme-cache/src/install-nvme-cache.sh
@@ -96,15 +96,24 @@ if [ "$HASBOOTED" = "no" ]; then
   cp -vf xxd /usr/sbin/
   cp -vf gawk /usr/sbin/
   cp -vf synofileutil /usr/sbin/
-  cp -vf libreadline.so.8.0 /lib/
-  cp -vf libsynocore.so.7 /lib/
   chmod 755 /usr/sbin/readlink /usr/sbin/xxd /usr/sbin/gawk /usr/sbin/synofileutil
-  chmod 644 /lib/libreadline.so.8.0 /lib/libsynocore.so.7
+
   ln -s /usr/sbin/gawk /usr/sbin/awk
   ln -s /usr/sbin/synofileutil /usr/sbin/synosetkeyvalue
+  
+  cp -vf libreadline.so.8.0 /lib/
+  cp -vf libsynocore.so.7 /lib/
+  cp -vf libmpfr.so.6.0.1 /lib/
+  cp -vf libsynocredentials.so.7 /lib/
+  chmod 644 /lib/libreadline.so.8.0 /lib/libsynocore.so.7 /lib/libmpfr.so.6.0.1 /lib/libsynocredentials.so.7
+
   ln -s /lib/libreadline.so.8.0 /lib/libreadline.so.8
   ln -s /lib/libreadline.so.8 /lib/libreadline.so
   ln -s /lib/libsynocore.so.7 /lib/libsynocore.so
+
+  ln -s /lib/libmpfr.so.6.0.1 /lib/libmpfr.so.6
+  ln -s /lib/libmpfr.so.6.0.1 /lib/libmpfr.so
+  ln -s /lib/libsynocredentials.so.7 /lib/libsynocredentials.so
 
   active_nvme
 elif [ "$HASBOOTED" = "yes" ]; then
