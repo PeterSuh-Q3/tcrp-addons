874f332122cfbf7a23e31662d2171307b2d13ebb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-07-01 20:49:53 +0900

    Update install.sh

--- reboottotcrp/src/install.sh
+++ reboottotcrp/src/install.sh
@@ -13,8 +13,12 @@ elif [ "$HASBOOTED" = "yes" ]; then
   cp -vf tcrp-reboot.sh /tmpRoot/usr/sbin/tcrp-reboot.sh
   chmod 755 /tmpRoot/usr/sbin/tcrp-reboot.sh
   if [ -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
-    echo "insert RebootToTcrp task"
-    /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "INSERT INTO task VALUES('RebootToTcrp', '', '-', '', 0, 0, 0, 0, '', 0, '/usr/sbin/tcrp-reboot.sh', 'script', '{}', '', '', '{}', '{}');"
+    if [ $(/tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "select count(*) as cnt from task a where task_name = 'RebootToTcrp';") -gt "0" ]; then
+      echo "A RebootToTcrp task already exists at task_name. skipped!!!"
+    else
+      echo "insert RebootToTcrp task"
+      /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "INSERT INTO task VALUES('RebootToTcrp', '', '-', '', 0, 0, 0, 0, '', 0, '/usr/sbin/tcrp-reboot.sh', 'script', '{}', '', '', '{}', '{}');"
+    fi
   else
     echo "copy RebootToTcrp task db"
     mkdir -p /tmpRoot/usr/syno/etc/esynoscheduler
