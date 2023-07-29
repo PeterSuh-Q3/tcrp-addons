be3853fd49b2fd4cc704f1a5838c38c5dcb6916f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-07-29 14:56:07 +0900

    Update installondsm.sh

--- nvme-cache/installondsm.sh
+++ nvme-cache/installondsm.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 echo "Installing NVMe cache enabler service"
-curl -kL https://github.com/PeterSuh-Q3/tcrp-addons/raw/main/nvme-cache/src/nvme-cache.sh -o /usr/sbin/nvme-cache.sh
-chmod 755 /usr/sbin/nvme-cache.sh
+curl -kL https://github.com/PeterSuh-Q3/tcrp-addons/raw/main/nvme-cache/nvme-service.sh -o /usr/sbin/nvme-service.sh
+chmod 755 /usr/sbin/nvme-service.sh
 cat > /etc/systemd/system/nvme-cache.service <<'EOF'
 [Unit]
 Description=NVMe cache enabler schedule
 [Service]
 Type=oneshot
-ExecStart=/usr/sbin/nvme-cache.sh
+ExecStart=/usr/sbin/nvme-service.sh
 [Install]
 WantedBy=multi-user.target
 EOF
