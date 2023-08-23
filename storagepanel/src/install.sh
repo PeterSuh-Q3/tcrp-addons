3db536984cbd574b656e1a8471a9eebeac75cdfa
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-08-23 21:41:56 +0900

    Add files via upload

--- storagepanel/src/install.sh
+++ storagepanel/src/install.sh
@@ -4,7 +4,7 @@ if [ "${1}" = "late" ]; then
   echo "Installing daemon for storagepanel"
   cp -v ./storagepanel.sh /tmpRoot/usr/sbin/storagepanel.sh
   shift
-  DEST="/tmpRoot/usr/lib/systemd/system/storagepanel.service"
+  DEST="/tmpRoot/etc/systemd/system/storagepanel.service"
   echo "[Unit]"                                          >${DEST}
   echo "Description=Modify storage panel"               >>${DEST}
   echo "After=multi-user.target"                        >>${DEST}
@@ -12,11 +12,11 @@ if [ "${1}" = "late" ]; then
   echo "[Service]"                                      >>${DEST}
   echo "Type=oneshot"                                   >>${DEST}
   echo "RemainAfterExit=true"                           >>${DEST}
-  echo "ExecStart=/usr/sbin/storagepanel.sh $@"          >>${DEST}
+  echo "ExecStart=/usr/sbin/storagepanel.sh $HDD_Bay $SSD_Bay"   >>${DEST}
   echo                                                  >>${DEST}
   echo "[Install]"                                      >>${DEST}
   echo "WantedBy=multi-user.target"                     >>${DEST}
 
   mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -vsf /usr/lib/systemd/system/storagepanel.service /tmpRoot/lib/systemd/system/multi-user.target.wants/storagepanel.service
+  ln -vsf /etc/systemd/system/storagepanel.service /tmpRoot/lib/systemd/system/multi-user.target.wants/storagepanel.service
 fi
