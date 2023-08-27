938b8f462ca3a45ad8de3c859f6b7726ca4faa32
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-08-27 12:40:54 +0900

    Add files via upload

--- nvmevolume/src/install.sh
+++ nvmevolume/src/install.sh
@@ -13,7 +13,7 @@ if [ "${1}" = "late" ]; then
   echo "[Service]"                                >>${DEST}
   echo "Type=oneshot"                             >>${DEST}
   echo "RemainAfterExit=true"                     >>${DEST}
-  echo "ExecStart=/usr/sbin/nvmevolume.sh"         >>${DEST}
+  echo "ExecStart=echo 'n' |/usr/sbin/nvmevolume.sh"   >>${DEST}
   echo                                            >>${DEST}
   echo "[Install]"                                >>${DEST}
   echo "WantedBy=multi-user.target"               >>${DEST}
