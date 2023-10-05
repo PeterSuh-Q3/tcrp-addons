f7d2d49d1ef94b9bd90bfded1d87883a6ce01265
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-05 10:44:03 +0900

    Update install.sh

--- syno-hdd-db/src/install.sh
+++ syno-hdd-db/src/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 model=$(uname -u | cut -d '_' -f3)
-echo model "$model" >&2  # debug
+echo model "${model}" >&2  # debug
 
 if [ "${1}" = "modules" ]; then
 
@@ -42,16 +42,16 @@ if [ "${1}" = "modules" ]; then
     
         # Get drive model
         hdmodel=$(cat "${1}/device/model")
-        hdmodel=$(printf "%s" "$hdmodel" | xargs)  # trim leading and trailing white space
+        hdmodel=$(printf "%s" "${hdmodel}" | xargs)  # trim leading and trailing white space
 
         # Fix dodgy model numbers
-        if [ $(echo  "$hdmodel" | grep Virtual | wc -l) -eq 0 ]; then
-            fixdrivemodel "$hdmodel"
+        if [ $(echo  "${hdmodel}" | grep Virtual | wc -l) -eq 0 ]; then
+            fixdrivemodel "${hdmodel}"
         fi
 
         # Get drive firmware version
         #fwrev=$(cat "${1}/device/rev")
-        #fwrev=$(printf "%s" "$fwrev" | xargs)  # trim leading and trailing white space
+        #fwrev=$(printf "%s" "${fwrev}" | xargs)  # trim leading and trailing white space
 
         device="/dev/$(basename -- "${1}")"
         # Account for SSD drives with spaces in their model name/number
@@ -64,25 +64,32 @@ if [ "${1}" = "modules" ]; then
             fwrev="1.13.2"
           else
             if [ ${REVISION} = "#42218" ]; then
-                fwrev=$(./hdparm701 -I "$device" | grep Firmware | cut -d':' -f2- | cut -d ' ' -f 3 )
+                fwrev=$(./hdparm701 -I "${device}" | grep Firmware | cut -d':' -f2- | cut -d ' ' -f 3 )
             elif [ ${REVISION} = "#42962" ]; then
-                fwrev=$(./hdparm711 -I "$device" | grep Firmware | cut -d':' -f2- | cut -d ' ' -f 3 )
+                fwrev=$(./hdparm711 -I "${device}" | grep Firmware | cut -d':' -f2- | cut -d ' ' -f 3 )
             else
-                fwrev=$(./hdparm720 -I "$device" | grep Firmware | cut -d':' -f2- | cut -d ' ' -f 3 )
+                fwrev=$(./hdparm720 -I "${device}" | grep Firmware | cut -d':' -f2- | cut -d ' ' -f 3 )
             fi
           fi  
         elif [[ $2 == "nvme" ]]; then
             fwrev=$(cat "$1/device/firmware_rev")
         fi
 
-        echo hdmodel "$hdmodel" >&2  # debug
-        echo fwrev "$fwrev" >&2      # debug
+        echo hdmodel "${hdmodel}" >&2  # debug
+        echo fwrev "${fwrev}" >&2      # debug
         
-        if [ -n "$hdmodel" ] && [ -n "$fwrev" ]; then
-            echo "Append drive and firmware:" >&2 # debug
-            jsondata='"'"$hdmodel"'":{"'"$fwrev"'":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]},
-            "default":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]}}' && echo $jsondata >> /etc/disk_db.json
-            echo "," >> /etc/disk_db.json
+        if [ -n "${hdmodel}" ] && [ -n "${fwrev}" ]; then
+#            if grep '"'"${hdmodel}"'":' /etc/disk_db.json >/dev/null; then
+               # Replace  "WD40PURX-64GVNY0":{  with  "WD40PURX-64GVNY0":{"80.00A80":{ ... }}},
+#                echo "Insert firmware version:"  # debug
+#                sed -i 's#"'"${hdmodel}"'":{#"'"${hdmodel}"'":{"'"${fwrev}"'":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]},#' /etc/disk_db.json
+#            else
+               # Add  "WD40PURX-64GVNY0":{"80.00A80":{ ... }}},"default":{ ... }}}
+                echo "Append drive and firmware:"  # debug
+                jsondata='"'"${hdmodel}"'":{"'"${fwrev}"'":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]},
+                "default":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]}}' && echo $jsondata >> /etc/disk_db.json
+                echo "," >> /etc/disk_db.json
+#            fi                    
         fi
     fi
   }
@@ -105,12 +112,12 @@ if [ "${1}" = "modules" ]; then
   # Host db files
   dbpath="/var/lib/disk-compatibility/"
   dbfile=$(ls "${dbpath}"*"${model}_host_v7.db")
-  echo dbfile "$dbfile" >&2  # debug
+  echo dbfile "${dbfile}" >&2  # debug
 
   diskdata=$(jq . /etc/disk_db.json)
-  jsonfile=$(jq '.disk_compatbility_info |= .+ '"$diskdata" $dbfile) && echo $jsonfile | jq . > $dbfile
+  jsonfile=$(jq '.disk_compatbility_info |= .+ '"$diskdata" ${dbfile}) && echo $jsonfile | jq . > ${dbfile}
   # print last 8 elements
-  jq '.disk_compatbility_info | to_entries | map(select(.value != null)) | .[-8:]' $dbfile
+  jq '.disk_compatbility_info | to_entries | map(select(.value != null)) | .[-8:]' ${dbfile}
 
   cp -vf ${dbfile} /etc/
   
