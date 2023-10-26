9ab93390b49eed3c044155382f13639dcba2e7ee
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-27 00:10:50 +0900

    Update install.sh

--- syno-hdd-db/src/install.sh
+++ syno-hdd-db/src/install.sh
@@ -117,7 +117,7 @@ if [ "${1}" = "modules" ]; then
   diskdata=$(jq . /etc/disk_db.json)
   jsonfile=$(jq '.disk_compatbility_info |= .+ '"$diskdata" ${dbfile}) && echo $jsonfile | jq . > ${dbfile}
   # print last 8 elements
-  jq '.disk_compatbility_info | to_entries | map(select(.value != null)) | .[-8:]' ${dbfile}
+  #jq '.disk_compatbility_info | to_entries | map(select(.value != null)) | .[-8:]' ${dbfile}
 
   cp -vf ${dbfile} /etc/
   
