43bcc6c27d7407c021dda8936a64a2afe1a3b399
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-28 23:52:36 +0900

    Create add_rev.sh

---
+++ add_rev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Please provide a revision as an argument."
+  exit 1
+fi
+
+rev="$1"
+baseurl="https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master"  # Base URL
+
+ls -d */ | grep -v -e "9p" | while IFS= read -r dir; do
+  for basemodel in `cat models.72`
+  do 
+    model =$(echo "${basemodel}" | sed 's/DS//' | sed 's/RS//' | sed 's/+/p/' | sed 's/DVA/dva/' | sed 's/FS/fs/' | sed 's/SA/sa/' )
+    echo "Adding ${model}_${rev} to ${dir}rpext-index.json"
+
+    jsonfile="./${dir}rpext-index.json"
+    model_url="${baseurl}/${dir}recipes/universal.json"
+    jq --arg model "${model}_${rev}" --arg url "$model_url" '.releases += { ($model): $url }' "$jsonfile" > temp.json && mv temp.json "$jsonfile"    
+
+  done
+done
