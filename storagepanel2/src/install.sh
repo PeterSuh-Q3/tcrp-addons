54a54cc1f4913763478c0d7bf520a266e5ca6ad0
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-11-06 21:29:11 +0900

    Update install.sh

--- storagepanel2/src/install.sh
+++ storagepanel2/src/install.sh
@@ -53,6 +53,7 @@ if [ ! -f "${FILE_JS}" ]; then
   exit
 fi
 
+HDD_BAY_LIST=()
 HDD_BAY_LIST=(RACK_0_Bay RACK_2_Bay RACK_4_Bay RACK_8_Bay RACK_10_Bay RACK_12_Bay RACK_12_Bay_2 RACK_16_Bay RACK_20_Bay RACK_24_Bay RACK_60_Bay TOWER_1_Bay TOWER_2_Bay TOWER_4_Bay TOWER_4_Bay_J TOWER_4_Bay_S TOWER_5_Bay TOWER_6_Bay TOWER_8_Bay TOWER_12_Bay)
 
 if [ "${HDD_BAY}" = "-r" ]; then
