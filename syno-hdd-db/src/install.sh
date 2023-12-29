068bd594fb6b07dec58d54d24c75182b9ff9399a
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-12-29 19:23:56 +0900

    Update install.sh

--- syno-hdd-db/src/install.sh
+++ syno-hdd-db/src/install.sh
@@ -90,12 +90,12 @@ if [ "${1}" = "modules" ]; then
               if grep '"'"${hdmodel}"'":' /etc/disk_db.json >/dev/null; then
                  # Replace  "WD40PURX-64GVNY0":{  with  "WD40PURX-64GVNY0":{"80.00A80":{ ... }}},
                   echo "Insert firmware version:"  # debug
-                  sed -i 's#"'"${hdmodel}"'":{#"'"${hdmodel}"'":{"'"${fwrev}"'":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]},#' /etc/disk_db.json
+                  sed -i 's#"'"${hdmodel}"'":{#"'"${hdmodel}"'":{"'"${fwrev}"'":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true,"smart_test_ignore":false,"smart_attr_ignore":false}]},#' /etc/disk_db.json
               else
                  # Add  "WD40PURX-64GVNY0":{"80.00A80":{ ... }}},"default":{ ... }}}
                   echo "Append drive and firmware:"  # debug
-                  jsondata='"'"${hdmodel}"'":{"'"${fwrev}"'":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]},
-                  "default":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true}]}}' && echo $jsondata >> /etc/disk_db.json
+                  jsondata='"'"${hdmodel}"'":{"'"${fwrev}"'":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true,"smart_test_ignore":false,"smart_attr_ignore":false}]},
+                  "default":{"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true,"smart_test_ignore":false,"smart_attr_ignore":false}]}}' && echo $jsondata >> /etc/disk_db.json
                   echo "," >> /etc/disk_db.json
               fi                    
           fi
