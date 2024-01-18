69cc265c785d257adf1f198e430c83d57681cc51
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-01-18 21:44:44 +0900

    Update sanrepair.sh

--- sanmanager-repair/src/sanrepair.sh
+++ sanmanager-repair/src/sanrepair.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 
 while true; do
     sleep 5
-    if [ -n "$(synopkg status ScsiTarget | grep error)" ]; then
+    status=$(synopkg status ScsiTarget)
+    if [ -n "$(echo "$status" | grep error)" ]; then
         modprobe target_core_mod
         modprobe target_core_iblock
         modprobe target_core_file
@@ -15,8 +16,11 @@ while true; do
         modprobe tcm_loop
         modprobe vhost
         modprobe vhost_scsi
-    
+
         synopkg start ScsiTarget
-        break        
+    fi
+    status=$(synopkg status ScsiTarget)
+    if [ -z "$(echo "$status" | grep error)" ]; then
+        break
     fi
 done
