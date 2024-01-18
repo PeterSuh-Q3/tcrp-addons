1d37e5cceaab303759ca0ea516d204f1f6f7d5dd
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-01-18 21:35:20 +0900

    Update sanrepair.sh

--- sanmanager-repair/src/sanrepair.sh
+++ sanmanager-repair/src/sanrepair.sh
@@ -4,22 +4,19 @@
 set -euo pipefail
 
 while true; do
-    if dmesg | grep -q "fuse init"; then
-        break
+    sleep 5
+    if [ -n "$(synopkg status ScsiTarget | grep error)" ]; then
+        modprobe target_core_mod
+        modprobe target_core_iblock
+        modprobe target_core_file
+        modprobe target_core_multi_file
+        modprobe target_core_user
+        modprobe iscsi_target_mod
+        modprobe tcm_loop
+        modprobe vhost
+        modprobe vhost_scsi
+    
+        synopkg start ScsiTarget
+        break        
     fi
-    sleep 1
 done
-
-if [ -n "$(synopkg status ScsiTarget | grep error)" ]; then
-    modprobe target_core_mod
-    modprobe target_core_iblock
-    modprobe target_core_file
-    modprobe target_core_multi_file
-    modprobe target_core_user
-    modprobe iscsi_target_mod
-    modprobe tcm_loop
-    modprobe vhost
-    modprobe vhost_scsi
-
-    synopkg start ScsiTarget
-fi
