33edc2842542b3e01c51d33ef0f196de90eaa6ec
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-01-30 22:48:11 +0900

    Update install.sh

--- sanmanager-repair/src/install.sh
+++ sanmanager-repair/src/install.sh
@@ -4,6 +4,17 @@ if [ "${1}" = "late" ]; then
   echo "Installing daemon for SAN MANAGER Repait Tool"
   cp -vf sanrepair.sh /tmpRoot/usr/sbin/sanrepair.sh
   chmod 755 /tmpRoot/usr/sbin/sanrepair.sh
+  cat > /tmpRoot/etc/systemd/system/sanrepair.timer <<'EOF'
+[Unit]
+Description=Configure SAN MANAGER Repair schedule
+[Timer]
+OnCalendar=*-*-* *:*:00
+Persistent=true
+[Install]
+WantedBy=timers.target
+EOF
+  mkdir -p /tmpRoot/etc/systemd/system/timers.target.wants
+  ln -sf /etc/systemd/system/sanrepair.timer /tmpRoot/etc/systemd/system/timers.target.wants/sanrepair.timer
   cat > /tmpRoot/etc/systemd/system/sanmanager-repair.service <<'EOF'
 [Unit]
 Description=Adds repair SAN MANAGER
