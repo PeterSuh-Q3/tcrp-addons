08595e8b89d00d0c5e0f4418c73bb64119d6a53f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-02-06 22:32:35 +0900

    Update install.sh

--- dbgutils/src/install.sh
+++ dbgutils/src/install.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env ash
 
 function saveLogs() {
-  #modprobe vfat
+  modprobe vfat
   echo 1 > /proc/sys/kernel/syno_install_flag
   mount /dev/synoboot1 /mnt
-  mkdir -p /mnt/logs/jr
-  cp /var/log/* /mnt/logs/jr
-  dmesg > /mnt/logs/jr/dmesg
+  mkdir -p /mnt/logs
+
+  rm -rf "/mnt/logs/${1}"
+  mkdir -p "/mnt/logs/${1}"
+  cp -vfR "/var/log/"* "/mnt/logs/${1}"
+  
+  dmesg >"/mnt/logs/${1}/dmesg.log"
+  lsmod >"/mnt/logs/${1}/lsmod.log"
+  lspci -Qnnk >"/mnt/logs/${1}/lspci.log" || true
+  ls -l /dev/ >"/mnt/logs/${1}/disk-dev.log" || true
+  ls -l /sys/class/scsi_host >"/mnt/logs/${1}/disk-scsi_host.log" || true
+  ls -l /sys/class/net/*/device/driver >"/mnt/logs/${1}/net-driver.log" || true
+    
+  echo "dbgutils - ${1}" > "/mnt/logs/${1}/${1}"
+  
   umount /mnt
 }
+
+[ -z "${1}" ] && echo "Usage: ${0} {early|jrExit|rcExit}" && exit 1
 
 if [ "${1}" = "early" ]; then
   echo "dbgutils - early"
@@ -17,12 +31,10 @@ if [ "${1}" = "early" ]; then
   
   #echo "Starting ttyd..."
   #/usr/sbin/ttyd /usr/bin/ash -l &
-elif [ "${1}" = "modules" ]; then
-  echo "dbgutils - modules"
+elif [ "${1}" = "jrExit" ]; then
+  echo "dbgutils - jrExit"
   saveLogs
-  echo "dbgutils - modules" > /mnt/logs/jr/modules
 elif [ "${1}" = "rcExit" ]; then
   echo "dbgutils - rcExit"
   saveLogs
-  echo "dbgutils - rcExit" > /mnt/logs/jr/rcExit  
 fi
