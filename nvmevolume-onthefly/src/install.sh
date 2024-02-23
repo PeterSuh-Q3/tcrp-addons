1e78796952cc3dcdde6304174e3657b10b3bf636
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-02-23 19:11:06 +0900

    Update install.sh

--- nvmevolume-onthefly/src/install.sh
+++ nvmevolume-onthefly/src/install.sh
@@ -46,6 +46,7 @@ function run_late() {
   echo "nvme-cache - late"
   echo "Copy libhwcontrol.so.1 file to tmpRoot"
   cp -vf /etc/libhwcontrol.so.1 ${tmpRoot}/lib64/
+  ln ${tmpRoot}/lib64/libhwcontrol.so.1 ${tmpRoot}/lib64/libhwcontrol.so
  #modify_synoinfo
 }
 
