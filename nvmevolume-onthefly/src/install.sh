7773044bbdf1f062a85625d0df7b9c236ea9f9ee
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-02-26 12:21:18 +0900

    Update install.sh

--- nvmevolume-onthefly/src/install.sh
+++ nvmevolume-onthefly/src/install.sh
@@ -4,20 +4,8 @@
 # | original    | 803e 00b8 0100 0000 7524 488b |
 # | patched     | 803e 00b8 0100 0000 9090 488b |
 
-# caution : In the case of ApolloLake, hexa detection is impossible if octets per line is specified as 256 bytes. It should be adjusted to 200.
-# xxd -c cols     format <cols> octets per line. Default 16 Max 256 (-i: 12, -ps: 30)
-
 tmpRoot="/tmpRoot"
 file="/lib64/libhwcontrol.so.1"
-PLATFORM="$(uname -u | cut -d '_' -f2)"
-
-echo "nvmevolume-onthefly - PLATFORM = ${PLATFORM}"
-
-if [ ${PLATFORM} = "apollolake" ]; then
-  cols="200"
-else
-  cols="256"
-fi  
 
 if [ "${1}" = "late" ]; then
   echo "nvmevolume-onthefly - ${1}"
