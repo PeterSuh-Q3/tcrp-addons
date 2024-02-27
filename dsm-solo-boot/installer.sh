cc9d8a18d6a37482691ea47c82f30d6a5a701684
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-02-27 12:49:49 +0900

    Update installer.sh

--- dsm-solo-boot/installer.sh
+++ dsm-solo-boot/installer.sh
@@ -46,7 +46,7 @@ dd if=/dev/synoboot1 of=/dev/sda5
 DoOrExit CREATE CreatePartition 6 151552 ${LINUX_FS_TYPE} ${ROOT_SKIP} ${DISKNODE}
 dd if=/dev/synoboot2 of=/dev/sda6
 DoOrExit CREATE CreatePartition 7 8087552 ${LINUX_FS_TYPE} ${ROOT_SKIP} ${DISKNODE}
-dd if=/dev/synoboot3 of=/dev/sda7
+dd if=/dev/synoboot3 of=/dev/sda7 status=progress bs=8M
 
 # boot on to /dev/sda5
 echo -e "a\n5\nw" | fdisk /dev/sda
