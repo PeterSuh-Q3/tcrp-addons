061d8def38721e5122d99d751aca8bf075cec62d
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-12 22:10:35 +0900

    Update install.sh

--- automount/src/install.sh
+++ automount/src/install.sh
@@ -51,5 +51,28 @@ if [ "${1}" = "modules" ]; then
   chmod 755 /usr/sbin/blkid /usr/sbin/sed /lib64/libblkid.so.1
 
 elif [ "${1}" = "patches" ]; then
+  LOADER_DISK=$(blkid | grep "6234-C863" | cut -c 1-8 | awk -F\/ '{print $3}')
+
+  BOOT_DISK="${LOADER_DISK}"
+  if [ -d /sys/block/${LOADER_DISK}/${LOADER_DISK}5 ]; then
+    for edisk in $(fdisk -l | grep "Disk /dev/sd" | awk '{print $2}' | sed 's/://' ); do
+        if [ $(fdisk -l | grep "fd Linux raid autodetect" | grep ${edisk} | wc -l ) -eq 3 ] && [ $(fdisk -l | grep "83 Linux" | grep ${edisk} | wc -l ) -eq 2 ]; then
+            echo "This is BASIC Type Disk & Has Syno Boot Partition. $edisk"
+            BOOT_DISK=$(echo "$edisk" | cut -c 6-8)
+        fi
+    done
+    if [ "${BOOT_DISK}" = "${LOADER_DISK}" ]; then
+        echo "Failed to find boot Partition on !!!"
+        exit 99
+    fi
+    p1="5"
+    p2="6"
+    p3="5"
+
+    [ -f ${BOOT_DISK}${p1} ] && ln -s /dev/${BOOT_DISK}${p1} /dev/synoboot1
+    [ -f ${BOOT_DISK}${p2} ] && ln -s /dev/${BOOT_DISK}${p2} /dev/synoboot2
+    [ -f ${LOADER_DISK}${p3} ] && ln -s /dev/${LOADER_DISK}${p3} /dev/synoboot3
+  
+  fi
   checkSynoboot
 fi
