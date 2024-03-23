291bdab3ccc9d7921d248085142c900dba85a2b6
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-23 23:23:19 +0900

    move /etc/extensionPorts proc to disks

--- nvme-cache/src/install-nvme-cache.sh
+++ nvme-cache/src/install-nvme-cache.sh
@@ -4,106 +4,10 @@ MODELS="DS918+ RS1619xs+ DS419+ DS1019+ DS719+ DS1621xs+"
 MODEL=$(cat /proc/sys/kernel/syno_hw_version)
 tmpRoot="/tmpRoot"
 
-function modify_synoinfo() {
-# add supportnvme="yes" , support_m2_pool="yes" to /etc/synoinfo.conf 2023.02.10
-  if [ -f ${tmpRoot}/etc/synoinfo.conf ]; then
-    echo 'add supportnvme="yes" to ${tmpRoot}/etc/synoinfo.conf'
-    if grep -q 'supportnvme' ${tmpRoot}/etc/synoinfo.conf; then
-      sed -i 's#supportnvme=.*#supportnvme="yes"#' ${tmpRoot}/etc/synoinfo.conf
-    else
-      echo 'supportnvme="yes"' >> ${tmpRoot}/etc/synoinfo.conf
-    fi
-    cat ${tmpRoot}/etc/synoinfo.conf | grep supportnvme
-      
-    echo 'add support_m2_pool="yes" to ${tmpRoot}/etc/synoinfo.conf'
-    if grep -q 'support_m2_pool' ${tmpRoot}/etc/synoinfo.conf; then
-      sed -i 's#support_m2_pool=.*#support_m2_pool="yes"#' ${tmpRoot}/etc/synoinfo.conf
-    else
-      echo 'support_m2_pool="yes"' >> ${tmpRoot}/etc/synoinfo.conf
-    fi
-    cat ${tmpRoot}/etc/synoinfo.conf | grep support_m2_pool
-  fi
-
-  if [ -f ${tmpRoot}/etc.defaults/synoinfo.conf ]; then
-    echo 'add supportnvme="yes" to ${tmpRoot}/etc.defaults/synoinfo.conf'
-    if grep -q 'supportnvme' ${tmpRoot}/etc.defaults/synoinfo.conf; then
-      sed -i 's#supportnvme=.*#supportnvme="yes"#' ${tmpRoot}/etc.defaults/synoinfo.conf
-    else
-      echo 'supportnvme="yes"' >> ${tmpRoot}/etc.defaults/synoinfo.conf
-    fi
-    cat ${tmpRoot}/etc.defaults/synoinfo.conf | grep supportnvme
-      
-    echo 'add support_m2_pool="yes" to ${tmpRoot}/etc.defaults/synoinfo.conf'
-    if grep -q 'support_m2_pool' ${tmpRoot}/etc.defaults/synoinfo.conf; then
-      sed -i 's#support_m2_pool=.*#support_m2_pool="yes"#' ${tmpRoot}/etc.defaults/synoinfo.conf
-    else
-      echo 'support_m2_pool="yes"' >> ${tmpRoot}/etc.defaults/synoinfo.conf
-    fi
-    cat ${tmpRoot}/etc.defaults/synoinfo.conf | grep support_m2_pool
-  fi
-}
-
-
-if [ "${1}" = "patches" ]; then
+if [ "${1}" = "late" ]; then
   echo "Installing addon nvmecache - ${1}"
 
-  BOOTDISK=""
-  devtype="$(blkid | grep "6234-C863" | cut -c 6-7 )"
-  if [ "${devtype}" = "sd" ]; then
-    BOOTDISK="$(blkid | grep "6234-C863" | cut -c 6-8 )"
-  elif [ "${devtype}" = "sa" ]; then
-    BOOTDISK="$(blkid | grep "6234-C863" | cut -c 6-10 )"
-  elif [ "${devtype}" = "nv" ]; then
-    BOOTDISK="$(blkid | grep "6234-C863" | cut -c 6-10 )"
-  else
-    BOOTDISK="synoboot"
-  fi
-  [ -n "${BOOTDISK}" ] && BOOTDISK_PHYSDEVPATH="$(cat /sys/block/${BOOTDISK}/uevent | grep 'PHYSDEVPATH' | cut -d'=' -f2)" || BOOTDISK_PHYSDEVPATH=""
-  echo "BOOTDISK=${BOOTDISK}"
-  echo "BOOTDISK_PHYSDEVPATH=${BOOTDISK_PHYSDEVPATH}"
-  rm -f /etc/nvmePorts
-  for P in $(ls -d /sys/block/nvme* 2>/dev/null); do
-    if [ -n "${BOOTDISK_PHYSDEVPATH}" -a "${BOOTDISK_PHYSDEVPATH}" = "$(cat ${P}/uevent | grep 'PHYSDEVPATH' | cut -d'=' -f2)" ]; then
-      echo "bootloader: ${P}"
-      continue
-    fi
-    PCIEPATH=$(cat ${P}/uevent 2>/dev/null | grep 'PHYSDEVPATH' | cut -d'/' -f4)
-    if [ -n "${PCIEPATH}" ]; then
-      # TODO: Need check?
-      MULTIPATH=$(cat ${P}/uevent 2>/dev/null | grep 'PHYSDEVPATH' | cut -d'/' -f5)
-      if [ -z "${MULTIPATH}" ]; then
-        echo "${PCIEPATH} does not support!"
-        continue
-      fi
-      echo "${PCIEPATH}" >>/etc/nvmePorts
-    fi
-  done
-  [ -f /etc/nvmePorts ] && cat /etc/nvmePorts
-  
-  if ! echo ${MODELS} | grep -q ${MODEL}; then
-    echo "${MODEL} use extensionPorts"
-    rm -f /etc/extensionPorts
-    echo "[pci]" >/etc/extensionPorts
-    chmod 755 /etc/extensionPorts
-    
-    num=1
-    while read -r N; do
-      echo "${num} - ${N}"
-      echo "pci${num}=\"${N}\"" >>/etc/extensionPorts
-      num=$((num + 1))
-    done < /etc/nvmePorts
-    cat /etc/extensionPorts
-  fi
-
-elif [ "${1}" = "late" ]; then
-  echo "Installing addon nvmecache - ${1}"
-
-  if ! echo ${MODELS} | grep -q ${MODEL}; then
-    echo "${MODEL} use extensionPorts"
-    cat /etc/extensionPorts
-    cp -vf /etc/extensionPorts ${tmpRoot}/etc/
-    cp -vf /etc/extensionPorts ${tmpRoot}/etc.defaults/
-  else
+  if echo ${MODELS} | grep -q ${MODEL}; then
   #
   # |       models      |     1st      |     2nd      |
   # | DS918+            | 0000:00:13.1 | 0000:00:13.2 |
@@ -148,5 +52,5 @@ elif [ "${1}" = "late" ]; then
       num=$((num + 1))
     done < /etc/nvmePorts
   fi
-  modify_synoinfo
+  
 fi
