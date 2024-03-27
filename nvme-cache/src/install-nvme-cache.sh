e621210908fd435514b28b8cfa59e3b324e51aca
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-27 23:15:24 +0900

    Update install-nvme-cache.sh

--- nvme-cache/src/install-nvme-cache.sh
+++ nvme-cache/src/install-nvme-cache.sh
@@ -4,7 +4,41 @@ MODELS="DS918+ RS1619xs+ DS419+ DS1019+ DS719+ DS1621xs+"
 MODEL=$(cat /proc/sys/kernel/syno_hw_version)
 tmpRoot="/tmpRoot"
 
-if [ "${1}" = "late" ]; then
+if [ "${1}" = "patches" ]; then
+  echo "Installing addon nvmecache - ${1}"
+
+  BOOTDISK=""
+  devtype="$(blkid | grep "6234-C863" | cut -c 6-7 )"
+  if [ "${devtype}" = "sd" ]; then
+    BOOTDISK="$(blkid | grep "6234-C863" | cut -c 6-8 )"
+  elif [ "${devtype}" = "sa" ]||[ "${devtype}" = "nv" ]||[ "${devtype}" = "mm" ]; then
+    BOOTDISK="$(blkid | grep "6234-C863" | cut -c 6-10 )"
+  else
+    BOOTDISK="synoboot"
+  fi
+  [ -n "${BOOTDISK}" ] && BOOTDISK_PHYSDEVPATH="$(cat /sys/block/${BOOTDISK}/uevent | grep 'PHYSDEVPATH' | cut -d'=' -f2)" || BOOTDISK_PHYSDEVPATH=""
+  echo "BOOTDISK=${BOOTDISK}"
+  echo "BOOTDISK_PHYSDEVPATH=${BOOTDISK_PHYSDEVPATH}"
+  rm -f /etc/nvmePorts
+  for P in $(ls -d /sys/block/nvme* 2>/dev/null); do
+    if [ -n "${BOOTDISK_PHYSDEVPATH}" -a "${BOOTDISK_PHYSDEVPATH}" = "$(cat ${P}/uevent | grep 'PHYSDEVPATH' | cut -d'=' -f2)" ]; then
+      echo "bootloader: ${P}"
+      continue
+    fi
+    PCIEPATH=$(cat ${P}/uevent 2>/dev/null | grep 'PHYSDEVPATH' | cut -d'/' -f4)
+    if [ -n "${PCIEPATH}" ]; then
+      # TODO: Need check?
+      MULTIPATH=$(cat ${P}/uevent 2>/dev/null | grep 'PHYSDEVPATH' | cut -d'/' -f5)
+      if [ -z "${MULTIPATH}" ]; then
+        echo "${PCIEPATH} does not support!"
+        continue
+      fi
+      echo "${PCIEPATH}" >>/etc/nvmePorts
+    fi
+  done
+  [ -f /etc/nvmePorts ] && cat /etc/nvmePorts
+
+elif [ "${1}" = "late" ]; then
   echo "Installing addon nvmecache - ${1}"
 
   if echo ${MODELS} | grep -q ${MODEL}; then
@@ -52,5 +86,4 @@ if [ "${1}" = "late" ]; then
       num=$((num + 1))
     done < /etc/nvmePorts
   fi
-  
 fi
