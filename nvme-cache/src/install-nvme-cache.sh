929fadc5b0b45b3bd0262e9034f2d82d15cea6d9
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-28 10:54:24 +0900

    Update install-nvme-cache.sh

--- nvme-cache/src/install-nvme-cache.sh
+++ nvme-cache/src/install-nvme-cache.sh
@@ -9,6 +9,7 @@ if [ "${1}" = "patches" ]; then
 
   BOOTDISK=""
   BOOTDISK_PART3=$(blkid -U "6234-C863" 2>/dev/null | sed 's/\/dev\///')
+  echo "BOOTDISK_PART3=${BOOTDISK_PART3}"
   [ -n "${BOOTDISK_PART3}" ] && BOOTDISK=$(ls -d /sys/block/*/${BOOTDISK_PART3} 2>/dev/null | cut -d'/' -f4)
   [ -n "${BOOTDISK}" ] && BOOTDISK_PHYSDEVPATH="$(cat /sys/block/${BOOTDISK}/uevent 2>/dev/null | grep 'PHYSDEVPATH' | cut -d'=' -f2)" || BOOTDISK_PHYSDEVPATH=""
   echo "BOOTDISK=${BOOTDISK}"
