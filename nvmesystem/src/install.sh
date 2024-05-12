32dda5f625b91fd7288e5b8965a8aae30fa0b699
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-05-12 21:31:14 +0900

    Add files via upload

--- nvmesystem/src/install.sh
+++ nvmesystem/src/install.sh
@@ -7,6 +7,7 @@
 #
 # From：jim3ma, https://jim.plus/blog/post/jim/synology-installation-with-nvme-disks-only
 #
+tmpRoot="/tmpRoot"
 
 if [ "${1}" = "early" ]; then
   echo "Installing addon nvmesystem - ${1}"
@@ -32,26 +33,23 @@ elif [ "${1}" = "late" ]; then
   echo "Installing addon nvmesystem - ${1}"
 
   # System volume is assembled with SSD Cache only, please remove SSD Cache and then reboot
-  sed -i "s/support_ssd_cache=.*/support_ssd_cache=\"no\"/" /tmpRoot/etc/synoinfo.conf /tmpRoot/etc.defaults/synoinfo.conf
+  ${tmpRoot}/usr/bin/sed -i "s/support_ssd_cache=.*/support_ssd_cache=\"no\"/" ${tmpRoot}/etc/synoinfo.conf ${tmpRoot}/etc.defaults/synoinfo.conf
 
   # disk/shared_disk_info_enum.c::84 Failed to allocate list in SharedDiskInfoEnum, errno=0x900.
-  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  SO_FILE="${tmpRoot}/usr/lib/libhwcontrol.so.1"
   [ ! -f "${SO_FILE}.bak" ] && cp -vf "${SO_FILE}" "${SO_FILE}.bak"
 
   cp -vf "${SO_FILE}" "${SO_FILE}.tmp"
-  xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
-    sed "s/0f95c00fb6c0488b9424081000006448/0f94c00fb6c0488b9424081000006448/; s/ffff89c18944240c8b44240809e84409/ffff89c18944240c8b44240890904409/" |
-    xxd -r -p >"${SO_FILE}" 2>/dev/null
+  ${tmpRoot}/usr/bin/xxd -c $(${tmpRoot}/usr/bin/xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
+    ${tmpRoot}/usr/bin/sed "s/0f95c00fb6c0488b9424081000006448/0f94c00fb6c0488b9424081000006448/; s/ffff89c18944240c8b44240809e84409/ffff89c18944240c8b44240890904409/" |
+    ${tmpRoot}/usr/bin/xxd -r -p >"${SO_FILE}" 2>/dev/null
   rm -f "${SO_FILE}.tmp"
 
   # Create storage pool page without RAID type.
-  cp -vf nvmesystem.sh /tmpRoot/usr/sbin/nvmesystem.sh
-  chmod +x /tmpRoot/usr/sbin/nvmesystem.sh
+  cp -vf nvmesystem.sh ${tmpRoot}/usr/sbin/nvmesystem.sh
+  chmod +x ${tmpRoot}/usr/sbin/nvmesystem.sh
 
-#  [ ! -f "/tmpRoot/usr/bin/gzip" ] && cp -vf gzip /tmpRoot/usr/bin/gzip
-#  chmod +x /tmpRoot/usr/bin/gzip
-
-  cat > /tmpRoot/etc/systemd/system/nvmesystem.service <<'EOF'
+  cat > ${tmpRoot}/etc/systemd/system/nvmesystem.service <<'EOF'
 [Unit]
 Description=Modify storage panel, from wjz304
 After=multi-user.target
@@ -65,7 +63,7 @@ ExecStart=/usr/sbin/nvmesystem.sh
 WantedBy=multi-user.target
 EOF
 
-  mkdir -vp /tmpRoot/etc/systemd/system/multi-user.target.wants
-  ln -vsf /etc/systemd/system/nvmesystem.service /tmpRoot/etc/systemd/system/multi-user.target.wants/nvmesystem.service
+  mkdir -vp ${tmpRoot}/etc/systemd/system/multi-user.target.wants
+  ln -vsf /etc/systemd/system/nvmesystem.service ${tmpRoot}/etc/systemd/system/multi-user.target.wants/nvmesystem.service
 
 fi
