b0ac24f8f532ec511a754bf02d11e9b0bee42913
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-05-14 20:58:03 +0900

    Update install.sh

--- nvmevolume-onthefly/src/install.sh
+++ nvmevolume-onthefly/src/install.sh
@@ -20,5 +20,5 @@ if [ "${1}" = "late" ]; then
     exit 0
   fi
   [ ! -f "${tmpRoot}${file}.bak" ] && cp -vf "${tmpRoot}${file}" "${tmpRoot}${file}.bak"
-  ${tmpRoot}/usr/bin/xxd -c 256 $(${tmpRoot}/usr/bin/xxd -p "${tmpRoot}${file}.bak" | wc -c) -p "${tmpRoot}${file}.bak" | sed "s/803e00b8010000007524488b/803e00b8010000009090488b/" | ${tmpRoot}/usr/bin/xxd -r -p > "${tmpRoot}${file}"
+  ${tmpRoot}/usr/bin/xxd -c $(${tmpRoot}/usr/bin/xxd -p "${tmpRoot}${file}.bak" | wc -c) -p "${tmpRoot}${file}.bak" | sed "s/803e00b8010000007524488b/803e00b8010000009090488b/" | ${tmpRoot}/usr/bin/xxd -r -p > "${tmpRoot}${file}"
 fi
