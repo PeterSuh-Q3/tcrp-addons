2a6084880c486a8051ba6c62d3739953ef19d3c2
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-06-04 20:25:43 +0900

    Update install.sh

--- autorecover/src/install.sh
+++ autorecover/src/install.sh
@@ -5,8 +5,11 @@ if [ "${1}" = "rcExit" ]; then
   if [ $(cat /var/log/linuxrc.syno.log | grep smallfixnumber | wc -l) -gt 0 ] && [ $(cat /var/log/junior_reason | grep -e error -e [7] | wc -l) -gt 0 ]; then
     echo "smallfixnumber difference detected. Automatic patching is performed. !!!"
     echo "Copy the rd.gz and zImage files from /tmpRoot where /dev/md0 is mounted."
-    mkdir -p /mnt/p2
+
+    mkdir -p /mnt/p1
+    mkdir -p /mnt/p2    
     cd /dev
+    mount -t vfat synoboot1 /mnt/p1
     mount -t vfat synoboot2 /mnt/p2
     
     mount_point="/tmpR" # Set the mount point
@@ -38,14 +41,21 @@ if [ "${1}" = "rcExit" ]; then
         break
       fi
     done
+
+    if [ $( mount | grep ${mount_point} | wc -l ) -gt 0 ]; then
+      # If the mount is successful, print a success message
+      echo "$device has been successfully mounted on $mount_point"
+      
+      cp -vf /tmpR/.syno/patch/rd.gz /mnt/p2
+      cp -vf /tmpR/.syno/patch/zImage /mnt/p2
+      cp -vf /tmpR/.syno/patch/grub_cksum.syno /mnt/p2
+  
+      if [ $? -eq 0 ]; then
+        [ $(cat /mnt/p1/boot/grub/grub.cfg | grep JOT | wc -l) -gt 0 ] && sed -i "s/set default=\"[0-9]\"/set default=\"2\"/g" /mnt/p1/boot/grub/grub.cfg
+        echo "The copy process is complete, Reboot Now..."
+        reboot
+      fi
+    fi
     
-    # If the mount is successful, print a success message
-    echo "$device has been successfully mounted on $mount_point"
-    
-    cp -vf /tmpR/.syno/patch/rd.gz /mnt/p2
-    cp -vf /tmpR/.syno/patch/zImage /mnt/p2
-    cp -vf /tmpR/.syno/patch/grub_cksum.syno /mnt/p2
-    echo "The copy process is complete, Reboot Now..."
-    reboot
   fi
 fi
