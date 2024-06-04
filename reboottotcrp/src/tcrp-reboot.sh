6a128bff7fc7ab3821723294780f178768f9df1c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-06-04 23:26:10 +0900

    Update tcrp-reboot.sh

--- reboottotcrp/src/tcrp-reboot.sh
+++ reboottotcrp/src/tcrp-reboot.sh
@@ -6,12 +6,6 @@ fi
 cd /dev/
 mount -t vfat synoboot1 /mnt/tcrp-p1
 cd /mnt/tcrp-p1
-if [ $(cat /mnt/tcrp-p1/boot/grub/grub.cfg | grep Verbose | wc -l ) -gt 0 ]; then
-echo "For Jot Mode"
-sed -i "s/set default=\"[0-9]\"/set default=\"3\"/g" /mnt/tcrp-p1/boot/grub/grub.cfg
-else
-echo "For Friend Mode"
 sed -i "s/set default=\"[0-9]\"/set default=\"1\"/g" /mnt/tcrp-p1/boot/grub/grub.cfg
-fi
 cd /mnt
 umount /mnt/tcrp-p1
