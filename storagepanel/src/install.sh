26ab349e6ccf90994c1ec73d5f836d65f8c99d3e
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-08-11 09:45:59 +0900

    Update install.sh

--- storagepanel/src/install.sh
+++ storagepanel/src/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ash
 #
 # Copyright (C) 2022 Ing <https://github.com/wjz304>
 #
@@ -32,26 +32,8 @@
 
 if [ "${1}" = "late" ]; then
 
-  HDD_BAY="RACK_60_Bay"
-  SSD_BAY="1X4"
-
-HDD_BAY_LIST=(RACK_0_Bay RACK_2_Bay RACK_4_Bay RACK_8_Bay RACK_10_Bay RACK_12_Bay RACK_12_Bay_2 RACK_16_Bay RACK_20_Bay RACK_24_Bay RACK_60_Bay TOWER_1_Bay TOWER_2_Bay TOWER_4_Bay TOWER_4_Bay_J TOWER_4_Bay_S TOWER_5_Bay TOWER_6_Bay TOWER_8_Bay TOWER_12_Bay)
-
-if [ "${1}" = "-h" ]; then
-  echo "Use: ${0} [HDD_BAY [SSD_BAY]]"
-  echo "  HDD_BAY: ${HDD_BAY_LIST[@]}"
-  echo "  SSD_BAY: (row)X(column)"
-  echo "  -r: restore"
-  echo "  -h: help"
-  echo "  e.g.:"
-  echo "    ${0}                  - auto"
-  echo "    ${0} RACK_24_Bay      - HDD_BAY set to RACK_24_Bay, SSD_BAY auto"
-  echo "    ${0} RACK_24_Bay 1X8  - HDD_BAY set to RACK_24_Bay, SSD_BAY set to 1X8"
-  echo "    ${0} RACK_60_Bay 2X8  - HDD_BAY set to RACK_60_Bay, SSD_BAY set to 2X8"
-  echo "    ${0} -r               - restore"
-  echo "    ${0} -h               - help"
-  exit
-fi
+HDD_BAY="RACK_60_Bay"
+SSD_BAY="1X4"
 
 _UNIQUE="$(/bin/get_key_value /etc.defaults/synoinfo.conf unique)"
 _BUILD="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
@@ -66,12 +48,13 @@ FILE_GZ="${FILE_JS}.gz"
 
 [ ! -f "${FILE_GZ}" ] && echo "${FILE_GZ} file does not exist" && exit 0
 
-if [ "${1}" = "-r" ]; then
+HDD_BAY_LIST="RACK_0_Bay RACK_2_Bay RACK_4_Bay RACK_8_Bay RACK_10_Bay RACK_12_Bay RACK_12_Bay_2 RACK_16_Bay RACK_20_Bay RACK_24_Bay RACK_60_Bay TOWER_1_Bay TOWER_2_Bay TOWER_4_Bay TOWER_4_Bay_J TOWER_4_Bay_S TOWER_5_Bay TOWER_6_Bay TOWER_8_Bay TOWER_12_Bay"
+
+if [ "${HDD_BAY}" = "-r" ]; then
   if [ -f "${FILE_GZ}.bak" ]; then
     mv -f "${FILE_GZ}.bak" "${FILE_GZ}"
     gzip -dc "${FILE_GZ}" >"${FILE_JS}"
   fi
-  exit
 fi
 
 [ ! -f "${FILE_GZ}.bak" ] && cp -f "${FILE_GZ}" "${FILE_GZ}.bak"
