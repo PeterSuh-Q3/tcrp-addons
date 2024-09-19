890fde8f7ed2334aaf26b811ee66e1e57947782f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-09-19 11:52:05 +0900

    Update install_rd.sh

--- misc/releases/install_rd.sh
+++ misc/releases/install_rd.sh
@@ -6,4 +6,45 @@ if [ "${1}" = "modules" ]; then
     cp lsz /usr/sbin/sz
     chmod +x ttyd
     ./ttyd login > /dev/null 2>&1 &
+
+elif [ "${1}" = "rcExit" ]; then
+  echo "Installing addon misc - ${1}"
+
+  mkdir -p /usr/syno/web/webman
+  # clear system disk space
+  cat >/usr/syno/web/webman/clean_system_disk.cgi <<EOF
+#!/bin/sh
+
+echo -ne "Content-type: text/plain; charset=\"UTF-8\"\r\n\r\n"
+if [ -b /dev/md0 ]; then
+  mkdir -p /mnt/md0
+  mount /dev/md0 /mnt/md0/
+  rm -rf /mnt/md0/@autoupdate/*
+  rm -rf /mnt/md0/upd@te/*
+  rm -rf /mnt/md0/.log.junior/*
+  umount /mnt/md0/
+  rm -rf /mnt/md0/
+  echo '{"success": true}'
+else
+  echo '{"success": false}'
+fi
+EOF
+  chmod +x /usr/syno/web/webman/clean_system_disk.cgi
+
+  # get logs
+  cat >/usr/syno/web/webman/get_logs.cgi <<EOF
+#!/bin/sh
+
+echo -ne "Content-type: text/plain; charset=\"UTF-8\"\r\n\r\n"
+echo "==== proc cmdline ===="
+cat /proc/cmdline 
+echo "==== SynoBoot log ===="
+cat /var/log/linuxrc.syno.log
+echo "==== Installerlog ===="
+cat /tmp/installer_sh.log
+echo "==== Messages log ===="
+cat /var/log/messages
+EOF
+  chmod +x /usr/syno/web/webman/get_logs.cgi
+  
 fi
