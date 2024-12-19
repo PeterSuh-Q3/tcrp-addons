f309d9d3c7e0484b72530510372c295d7187aeb9
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-12-19 12:52:42 +0900

    add force ifcfg-eth7

--- smb3-multi/src/smb3-multi.sh
+++ smb3-multi/src/smb3-multi.sh
@@ -4,3 +4,7 @@ echo "Add the smb3 multichannel configuration to the /etc/samba/smb.conf file."
 [ $(cat /etc/samba/smb.conf | grep "aio read size=1" | wc -l) -eq 0 ] && echo "aio read size=1" >> /etc/samba/smb.conf
 [ $(cat /etc/samba/smb.conf | grep "aio write size=1" | wc -l) -eq 0 ] && echo "aio write size=1" >> /etc/samba/smb.conf
 cat /etc/samba/smb.conf | grep -e multi -e aio
+
+#add force ifcfg-eth7
+echo -e "DEVICE=eth7\nBOOTPROTO=dhcp\nONBOOT=yes\nIPV6INIT=off" >"/etc/sysconfig/network-scripts/ifcfg-eth7"
+/etc/rc.network restart
