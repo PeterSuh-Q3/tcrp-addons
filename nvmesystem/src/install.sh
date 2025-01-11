a418fe02d6258ea2c7d9d730318800c55421f5c6
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-01-11 21:17:34 +0900

    Fix Something

--- nvmesystem/src/install.sh
+++ nvmesystem/src/install.sh
@@ -5,9 +5,20 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# From:jim3ma, https://jim.plus/blog/post/jim/synology-installation-with-nvme-disks-only
+# From：jim3ma, https://jim.plus/blog/post/jim/synology-installation-with-nvme-disks-only
 #
-tmpRoot="/tmpRoot"
+
+# PLATFORMS="epyc7002"
+# PLATFORM="$(/bin/get_key_value /etc.defaults/synoinfo.conf unique | cut -d"_" -f2)"
+# if ! echo "${PLATFORMS}" | grep -wq "${PLATFORM}"; then
+#   echo "${PLATFORM} is not supported nvmesystem addon!"
+#   exit 0
+# fi
+_BUILD="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
+if [ ${_BUILD:-64570} -lt 69057 ]; then
+  echo "${_BUILD} is not supported nvmesystem addon!"
+  exit 0
+fi
 
 PLATFORMS="epyc7002 geminilake v1000 r1000"
 PLATFORM="$(/bin/get_key_value /etc.defaults/synoinfo.conf unique | cut -d"_" -f2)"
@@ -25,56 +36,53 @@ if [ "${1}" = "early" ]; then
 
   # [CREATE][failed] Raidtool initsys
   SO_FILE="/usr/syno/bin/scemd"
-  [ ! -f "${SO_FILE}.bak" ] && cp -vf "${SO_FILE}" "${SO_FILE}.bak"
+  [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
   cp -f "${SO_FILE}" "${SO_FILE}.tmp"
-  xxd -c $(xxd -p "${SO_FILE}.tmp" | wc -c) -p "${SO_FILE}.tmp" |
+  xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
     sed "s/4584ed74b7488b4c24083b01/4584ed75b7488b4c24083b01/" |
-    xxd -r -p >"${SO_FILE}"
+    xxd -r -p >"${SO_FILE}" 2>/dev/null
   rm -f "${SO_FILE}.tmp"
 
 elif [ "${1}" = "late" ]; then
   echo "Installing addon nvmesystem - ${1}"
 
-  # System volume is assembled with SSD Cache only, please remove SSD Cache and then reboot
-  ${tmpRoot}/usr/bin/sed -i "s/support_ssd_cache=.*/support_ssd_cache=\"no\"/" ${tmpRoot}/etc/synoinfo.conf ${tmpRoot}/etc.defaults/synoinfo.conf
-
   # disk/shared_disk_info_enum.c::84 Failed to allocate list in SharedDiskInfoEnum, errno=0x900.
-  SO_FILE="${tmpRoot}/usr/lib/libhwcontrol.so.1"
-  [ ! -f "${SO_FILE}.bak" ] && cp -vf "${SO_FILE}" "${SO_FILE}.bak"
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
 
-  cp -vf "${SO_FILE}" "${SO_FILE}.tmp"
+  cp -pf "${SO_FILE}" "${SO_FILE}.tmp"
   if echo "${PLATFORMS}" | grep -qw "${PLATFORM}"; then
-    ${tmpRoot}/usr/bin/xxd -c $(${tmpRoot}/usr/bin/xxd -p "${SO_FILE}.tmp" | wc -c) -p "${SO_FILE}.tmp" | 
-      sed "s/0f95c00fb6c0488b94240810/0f94c00fb6c0488b94240810/; s/8944240c8b44240809e84409/8944240c8b44240890904409/" | 
-      ${tmpRoot}/usr/bin/xxd -r -p > "${SO_FILE}"
+      xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
+      sed "s/0f95c00fb6c0488b94240810/0f94c00fb6c0488b94240810/; s/8944240c8b44240809e84409/8944240c8b44240890904409/" |
+      xxd -r -p >"${SO_FILE}" 2>/dev/null
   else    
     # Activate nvmevolume for Non-DT flatforms
-    ${tmpRoot}/usr/bin/xxd -c $(${tmpRoot}/usr/bin/xxd -p "${SO_FILE}.tmp" | wc -c) -p "${SO_FILE}.tmp" | 
+      xxd -c $(xxd -p "${SO_FILE}.tmp" | wc -c) -p "${SO_FILE}.tmp" | 
       sed "s/803e00b801000000752.488b/803e00b8010000009090488b/" | 
-      ${tmpRoot}/usr/bin/xxd -r -p > "${SO_FILE}"
+      xxd -r -p > "${SO_FILE}"
   fi  
   rm -f "${SO_FILE}.tmp"  
 
   # Create storage pool page without RAID type.
-  cp -vf nvmesystem.sh ${tmpRoot}/usr/sbin/nvmesystem.sh
-  chmod +x ${tmpRoot}/usr/sbin/nvmesystem.sh
+  cp -vpf nvmesystem.sh /tmpRoot/usr/sbin/nvmesystem.sh
+  chmod +x /tmpRoot/usr/sbin/nvmesystem.sh
 
-  cat > ${tmpRoot}/etc/systemd/system/nvmesystem.service <<'EOF'
+  cat > /tmpRoot/etc/systemd/system/nvmesystem.service <<'EOF'
 [Unit]
-Description=Modify storage panel, from wjz304
+Description=mshell addon nvmesystem daemon
 Wants=smpkg-custom-install.service pkgctl-StorageManager.service
 After=smpkg-custom-install.service
 After=storagepanel.service
 [Service]
 Type=oneshot
-RemainAfterExit=true
+RemainAfterExit=yes
 ExecStart=/usr/sbin/nvmesystem.sh
 [Install]
 WantedBy=multi-user.target
 EOF
 
-  mkdir -vp ${tmpRoot}/etc/systemd/system/multi-user.target.wants
-  ln -vsf /etc/systemd/system/nvmesystem.service ${tmpRoot}/etc/systemd/system/multi-user.target.wants/nvmesystem.service
+  mkdir -vp /tmpRoot/etc/systemd/system/multi-user.target.wants
+  ln -vsf /etc/systemd/system/nvmesystem.service /tmpRoot/etc/systemd/system/multi-user.target.wants/nvmesystem.service
 
 elif [ "${1}" = "rcExit" ]; then
   echo "Installing addon nvmesystem - ${1}"
