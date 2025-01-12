e85d76b5663fed60c9a6bf2e439fb42fc607c110
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-01-12 22:15:59 +0900

    Update install.sh

--- cpuinfo/src/install.sh
+++ cpuinfo/src/install.sh
@@ -4,7 +4,7 @@ if [ "${1}" = "late" ]; then
   echo "Installing daemon for CPU Info"
   cp -vf cpuinfo.sh /tmpRoot/usr/sbin/cpuinfo.sh
   chmod 755 /tmpRoot/usr/sbin/cpuinfo.sh
-  cat > /tmpRoot/etc/systemd/system/cpuinfo.service <<'EOF'
+  cat > /tmpRoot/usr/lib/systemd/system/cpuinfo.service <<'EOF'
 [Unit]
 Description=Adds correct CPU Info, from FOXBI
 After=multi-user.target
@@ -15,6 +15,6 @@ ExecStart=/usr/sbin/cpuinfo.sh
 [Install]
 WantedBy=multi-user.target  
 EOF
-  mkdir -p /tmpRoot/etc/systemd/system/multi-user.target.wants
-  ln -sf /etc/systemd/system/cpuinfo.service /tmpRoot/etc/systemd/system/multi-user.target.wants/cpuinfo.service
+  mkdir -p /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -sf /usr/lib/systemd/system/cpuinfo.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/cpuinfo.service
 fi
