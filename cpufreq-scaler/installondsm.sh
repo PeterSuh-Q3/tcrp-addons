2e54d14c5a532e325a5b946b6fa93fe795b3094c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-01-12 22:12:10 +0900

    Update installondsm.sh

--- cpufreq-scaler/installondsm.sh
+++ cpufreq-scaler/installondsm.sh
@@ -3,7 +3,7 @@
 echo "Installing ACPI cpufreq userspace scaler"
 cp -vf scaler.sh /usr/sbin/scaler.sh
 chmod 755 /usr/sbin/scaler.sh
-  cat > /etc/systemd/system/cpufreq-userspace-scaler.service <<'EOF'
+  cat > /usr/lib/systemd/system/cpufreq-userspace-scaler.service <<'EOF'
 [Unit]
 Description=ACPI cpufreq userspace scaler
 [Service]
@@ -15,5 +15,5 @@ ExecStart=/usr/sbin/scaler.sh
 [Install]
 WantedBy=multi-user.target
 EOF
-  mkdir -p /etc/systemd/system/multi-user.target.wants
-  ln -sf /etc/systemd/system/cpufreq-userspace-scaler.service /etc/systemd/system/multi-user.target.wants/cpufreq-userspace-scaler.service
+  mkdir -p /usr/lib/systemd/system/multi-user.target.wants
+  ln -sf /usr/lib/systemd/system/cpufreq-userspace-scaler.service /usr/lib/systemd/system/multi-user.target.wants/cpufreq-userspace-scaler.service
