35849d81a843bbb85038d6395e0fdf482cf836b5
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-02-18 20:25:01 +0900

    Update install.sh

--- nvmevolume-onthefly/src/install.sh
+++ nvmevolume-onthefly/src/install.sh
@@ -19,6 +19,9 @@ if [ "${1}" = "late" ]; then
   if [ "${MODEL}" = "DS3622xs+" ]; then
     echo "nvmevolume-onthefly - ${1}, Skip DS3622xs+ (Not Supported)"
     exit 0
+  elif [ "${MODEL}" = "DS3615xs" ]; then
+    echo "nvmevolume-onthefly - ${1}, Skip DS3615xs (Not Supported)"
+    exit 0
   fi
   [ ! -f "${tmpRoot}${file}.bak" ] && cp -vf "${tmpRoot}${file}" "${tmpRoot}${file}.bak"
   ${tmpRoot}/usr/bin/xxd -c $(${tmpRoot}/usr/bin/xxd -p "${tmpRoot}${file}.bak" | wc -c) -p "${tmpRoot}${file}.bak" | sed "s/803e00b801000000752.488b/803e00b8010000009090488b/" | ${tmpRoot}/usr/bin/xxd -r -p > "${tmpRoot}${file}"
