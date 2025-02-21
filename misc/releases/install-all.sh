823f282f3a3318fbf7c1c25638cb7d07b1db13f4
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-02-21 22:03:50 +0900

    Update install-all.sh

--- misc/releases/install-all.sh
+++ misc/releases/install-all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -o pipefail 
+
 PLATFORM="$(uname -u | cut -d '_' -f2)"
 
 SED_PATH='/tmpRoot/usr/bin/sed'
@@ -68,30 +70,42 @@ fixnvidia() {
 
 fixintelgpu() {
   # Intel GPU
-  echo "replace ingel gpu info for i915le10th"
+  echo "replace intel gpu info for i915le10th"
 
-  GPU="$(lspci -n 2>/dev/null | grep 0300 | grep 8086 | cut -d' ' -f3 | sed 's/://g')"
-  [ -z "${GPU}" -o $(echo -n "${GPU}" | wc -c) -ne 8 ] && echo "GPU is not detected" && exit 0
+  GPU="$(lspci -nd ::300 2>/dev/null | grep 8086 | cut -d' ' -f3 | sed 's/://g')"
+  grep -iq "${GPU}" "/usr/sbin/i915ids" 2>/dev/null || GPU=""
+  if [ -z "${GPU}" ] || [ $(echo -n "${GPU}" | wc -c) -ne 8 ]; then
+    echo "GPU is not detected"
+    exit 0
+  fi
 
   KO_FILE="/usr/lib/modules/i915.ko"
-  [ ! -f "${KO_FILE}" ] && echo "i915.ko does not exist" && exit 0
+  if [ ! -f "${KO_FILE}" ]; then
+    echo "i915.ko does not exist"
+    exit 0
+  fi  
 
-  if grep -iq ${GPU} "/usr/sbin/i915ids" 2>/dev/null; then
-    isLoad=0
-    if lsmod 2>/dev/null | grep -q ^i915; then
-      isLoad=1
-      /usr/sbin/modprobe -r i915
-    fi
-    GPU_DEF="86800000923e0000"
-    GPU_BIN="${GPU:2:2}${GPU:0:2}0000${GPU:6:2}${GPU:4:2}0000"
-    echo "GPU:${GPU} GPU_BIN:${GPU_BIN}"
-    cp -vf "${KO_FILE}" "${KO_FILE}.bak"
-    cp -f "${KO_FILE}" "${KO_FILE}.tmp"
-    xxd -c $(xxd -p "${KO_FILE}.tmp" 2>/dev/null | wc -c) -p "${KO_FILE}.tmp" 2>/dev/null |
-      sed "s/${GPU_DEF}/${GPU_BIN}/; s/308201f706092a86.*70656e6465647e0a//" |
-      xxd -r -p >"${KO_FILE}" 2>/dev/null
-    rm -f "${KO_FILE}.tmp"
-    [ "${isLoad}" = "1" ] && /usr/sbin/modprobe i915
+  isLoad=0
+  if lsmod 2>/dev/null | grep -q "^i915"; then
+    isLoad=1
+    echo "removing i915 ..." 
+    /usr/sbin/modprobe -r i915
+  fi
+  GPU_DEF="86800000923e0000"
+  GPU_BIN="${GPU:2:2}${GPU:0:2}0000${GPU:6:2}${GPU:4:2}0000"
+  echo "GPU:${GPU} GPU_BIN:${GPU_BIN}"
+  cp -pf "${KO_FILE}" "${KO_FILE}.tmp"
+  if xxd -c $(xxd -p "${KO_FILE}.tmp" 2>/dev/null | wc -c) -p "${KO_FILE}.tmp" 2>/dev/null |
+    sed "s/${GPU_DEF}/${GPU_BIN}/; s/308201f706092a86.*70656e6465647e0a//" |
+    xxd -r -p >"${KO_FILE}" 2>/dev/null; then
+    echo "i915 xxd proc success!!!" 
+  else  
+    echo "i915 xxd proc fail!!!" 
+  fi  
+  rm -vf "${KO_FILE}.tmp"
+  if [ "${isLoad}" = "1" ]; then
+    echo "doing modprobe i915 ..." 
+    /usr/sbin/modprobe i915
   fi
   
 }
