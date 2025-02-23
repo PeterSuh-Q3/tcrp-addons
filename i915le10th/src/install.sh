47c88acd8e8e27e1b3b2f50cf8dbedf54f4f66ff
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-02-23 13:21:13 +0900

    Update install.sh

--- i915le10th/src/install.sh
+++ i915le10th/src/install.sh
@@ -1,48 +1,75 @@
-#!/usr/bin/env ash
+#!/bin/sh
 
-if [ "${1}" = "late" ]; then
-  echo "Installing daemon for i915le10th"
+set -o pipefail 
 
-  cp -vf ./i915ids /usr/sbin/i915ids
-  chmod +x /usr/sbin/i915ids
+PLATFORM="$(uname -u | cut -d '_' -f2)"
 
-  SED_PATH='/tmpRoot/usr/bin/sed'
-  XXD_PATH='/tmpRoot/usr/bin/xxd'
-  LSPCI_PATH='/tmpRoot/usr/bin/lspci'
-
+fixintelgpu() {
   # Intel GPU
-  if [ -f /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf ] && [ -f /tmpRoot/usr/lib/modules/i915.ko ]; then
-    export LD_LIBRARY_PATH=/tmpRoot/usr/bin:/tmpRoot/usr/lib:${LD_LIBRARY_PATH}
-    GPU="$(${LSPCI_PATH} -n | grep 0300 | grep 8086 | cut -d " " -f 3 | ${SED_PATH} -e 's/://g')"
-    echo "${GPU}" >/tmpRoot/root/i915.GPU
-    if [ -n "${GPU}" -a $(echo -n "${GPU}" | wc -c) -eq 8 ]; then
-      if [ $(grep -i ${GPU} /usr/sbin/i915ids | wc -l) -eq 0 ]; then
-        echo "Intel GPU is not detected (${GPU}), nothing to do"
-        #${SED_PATH} -i 's/^i915/# i915/g' /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf
-      else
-        GPU_DEF="86800000923e0000"
-        GPU_BIN="${GPU:2:2}${GPU:0:2}0000${GPU:6:2}${GPU:4:2}0000"
-        KO_SIZE="$(${XXD_PATH} -p /tmpRoot/usr/lib/modules/i915.ko | wc -c)"
-        ${XXD_PATH} -c ${KO_SIZE} -p /tmpRoot/usr/lib/modules/i915.ko /tmpRoot/root/i915.ko.hex
-        if [ $(grep -i "${GPU_BIN}" /tmpRoot/root/i915.ko.hex | wc -l) -gt 0 ]; then
-          echo "Intel GPU is detected (${GPU}), already support"
-        else
-          echo "Intel GPU is detected (${GPU}), replace id"
-          if [ ! -f /tmpRoot/usr/lib/modules/i915.ko.bak ]; then
-            cp -f /tmpRoot/usr/lib/modules/i915.ko /tmpRoot/usr/lib/modules/i915.ko.bak
-          fi
-          ${SED_PATH} -i "s/${GPU_DEF}/${GPU_BIN}/; s/308201f706092a86.*70656e6465647e0a//" /tmpRoot/root/i915.ko.hex
-          if [ -n "$(cat /tmpRoot/root/i915.ko.hex)" ]; then
-            ${XXD_PATH} -r -p /tmpRoot/root/i915.ko.hex >/tmpRoot/usr/lib/modules/i915.ko
-            rm -f /tmpRoot/root/i915.ko.hex
+  echo "replace intel gpu info for i915le10th"
 
-            rmmod i915
-            insmod /tmpRoot/usr/lib/modules/i915.ko
-          else
-            echo "Intel GPU is detected (${GPU}), replace i915.ko error"
-          fi
-        fi
-      fi
-    fi
+  GPU="$(lspci -nd ::300 2>/dev/null | grep 8086 | cut -d' ' -f3 | sed 's/://g')"
+  grep -iq "${GPU}" "/usr/sbin/i915ids" 2>/dev/null || GPU=""
+  if [ -z "${GPU}" ] || [ $(echo -n "${GPU}" | wc -c) -ne 8 ]; then
+    echo "GPU is not detected"
+    exit 0
   fi
+
+  KO_FILE="/usr/lib/modules/i915.ko"
+  if [ ! -f "${KO_FILE}" ]; then
+    echo "i915.ko does not exist"
+    exit 0
+  fi  
+
+  isLoad=0
+  if lsmod 2>/dev/null | grep -q "^i915"; then
+    isLoad=1
+    echo "removing i915 ..." 
+    /usr/sbin/modprobe -r i915
+  fi
+  GPU_DEF="86800000923e0000"
+  GPU_BIN="${GPU:2:2}${GPU:0:2}0000${GPU:6:2}${GPU:4:2}0000"
+  echo "GPU:${GPU} GPU_BIN:${GPU_BIN}"
+  cp -pf "${KO_FILE}" "${KO_FILE}.tmp"
+  if xxd -c $(xxd -p "${KO_FILE}.tmp" 2>/dev/null | wc -c) -p "${KO_FILE}.tmp" 2>/dev/null |
+    sed "s/${GPU_DEF}/${GPU_BIN}/; s/308201f706092a86.*70656e6465647e0a//" |
+    xxd -r -p >"${KO_FILE}" 2>/dev/null; then
+    echo "i915 xxd proc success!!!" 
+  else  
+    echo "i915 xxd proc fail!!!" 
+  fi  
+  rm -vf "${KO_FILE}.tmp"
+  if [ "${isLoad}" = "1" ]; then
+    echo "doing modprobe i915 ..." 
+    /usr/sbin/modprobe i915
+  fi
+  
+}
+
+copyintelgpu() {
+  KO_FILE="/tmpRoot/usr/lib/modules/i915.ko"
+  [ ! -f "${KO_FILE}.bak" ] && cp -vf "${KO_FILE}" "${KO_FILE}.bak"
+  cp -vf "/usr/lib/modules/i915.ko" "${KO_FILE}"
+}
+
+if [ "${1}" = "patches" ]; then
+    echo "Installing addon i915le1th - ${1}"
+
+    cp -vf /exts/misc/xxd /usr/bin/xxd
+    chmod +x /usr/bin/xxd
+
+    cp -vf /exts/misc/sed /usr/bin/sed
+    chmod +x /usr/bin/sed
+
+    cp -vf /exts/misc/i915ids /usr/sbin/i915ids
+    chmod +x /usr/sbin/i915ids
+
+    case "${PLATFORM}" in
+    apollolake)
+        fixintelgpu
+        ;;
+    geminilake)
+        fixintelgpu
+        ;;
+    esac
 fi
