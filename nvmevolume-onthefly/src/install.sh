b3093020f2154598adbc6b8e2f338835485447f8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-02-25 10:23:12 +0900

    Update install.sh

--- nvmevolume-onthefly/src/install.sh
+++ nvmevolume-onthefly/src/install.sh
@@ -20,6 +20,14 @@ if [ "${1}" = "late" ]; then
     echo "nvmevolume-onthefly - ${1}, Skip ${PLATFORM} (Not Supported)"
     exit 0
   fi
+
+  REVISION="$(uname -a | cut -d ' ' -f4)"
+  echo "REVISION = ${REVISION}"
+  if [ ${REVISION} = "#42218" ]; then
+    echo "nvmevolume-onthefly - ${1}, Skip ${REVISION} (Not Supported)"
+    exit 0
+  fi  
+    
   [ ! -f "${tmpRoot}${file}.bak" ] && cp -vf "${tmpRoot}${file}" "${tmpRoot}${file}.bak"
   ${tmpRoot}/usr/bin/xxd -c $(${tmpRoot}/usr/bin/xxd -p "${tmpRoot}${file}.bak" | wc -c) -p "${tmpRoot}${file}.bak" | sed "s/803e00b801000000752.488b/803e00b8010000009090488b/" | ${tmpRoot}/usr/bin/xxd -r -p > "${tmpRoot}${file}"
 fi
