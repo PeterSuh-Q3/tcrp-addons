179bcff8a9ac938250c7a1ce835148f8f49c4d2b
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-03-06 20:31:10 +0900

    Update tcrp-reboot.sh

--- reboottotcrp/src/tcrp-reboot.sh
+++ reboottotcrp/src/tcrp-reboot.sh
@@ -3,8 +3,8 @@ echo "Change the default of grub.cfg boot menu to Tinycore Loader Build in M-SHE
 if [ ! -d /mnt/tcrp-p1 ]; then
   mkdir /mnt/tcrp-p1
 fi  
-cd /dev/
-mount -t vfat synoboot1 /mnt/tcrp-p1
+
+mount -o loop /dev/synoboot1 /mnt/tcrp-p1
 cd /mnt/tcrp-p1
 sed -i "s/set default=\"[0-9]\"/set default=\"1\"/g" /mnt/tcrp-p1/boot/grub/grub.cfg
 cd /mnt
