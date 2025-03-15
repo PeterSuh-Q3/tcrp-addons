01aff26ddd76b2b026c4ccb22dda4b0fe587ed2c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-03-15 13:46:30 +0900

    Update install.sh

--- nvmesystem/src/install.sh
+++ nvmesystem/src/install.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env ash
+#!/usr/bin/env sh
 #
-# Copyright (C) 2023 PeterSuh-Q3 <https://github.com/PeterSuh-Q3>
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# From：jim3ma, https://jim.plus/blog/post/jim/synology-installation-with-nvme-disks-only
+# From: jim3ma, https://jim.plus/blog/post/jim/synology-installation-with-nvme-disks-only
 #
 
 # PLATFORMS="epyc7002"
@@ -14,23 +14,21 @@
 #   echo "${PLATFORM} is not supported nvmesystem addon!"
 #   exit 0
 # fi
-_BUILD="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
-if [ ${_BUILD:-64570} -lt 69057 ]; then
-  echo "${_BUILD} is not supported nvmesystem addon!"
-  exit 0
-fi
-
-PLATFORMS="epyc7002 geminilake v1000 r1000"
-PLATFORM="$(/bin/get_key_value /etc.defaults/synoinfo.conf unique | cut -d"_" -f2)"
+# _BUILD="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
+# if [ ${_BUILD:-42218} -lt 42218 ]; then
+#   echo "${_BUILD} is not supported nvmesystem addon!"
+#   exit 0
+# fi
 
 if [ "${1}" = "early" ]; then
   echo "Installing addon nvmesystem - ${1}"
 
+  # for mshell
   [ ! -f "/usr/sbin/xxd" ] && cp -vf xxd /usr/sbin/xxd
   chmod +x /usr/sbin/xxd
   [ ! -f "/usr/sbin/sed" ] && cp -vf sed /usr/sbin/sed
   chmod +x /usr/sbin/sed
-
+  
   # System volume is assembled with SSD Cache only, please remove SSD Cache and then reboot
   sed -i "s/support_ssd_cache=.*/support_ssd_cache=\"no\"/" /etc/synoinfo.conf /etc.defaults/synoinfo.conf
 
@@ -38,58 +36,65 @@ if [ "${1}" = "early" ]; then
   SO_FILE="/usr/syno/bin/scemd"
   [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
   cp -f "${SO_FILE}" "${SO_FILE}.tmp"
-  xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
-    sed "s/4584ed74b7488b4c24083b01/4584ed75b7488b4c24083b01/" |
+  xxd -c "$(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c)" -p "${SO_FILE}.tmp" 2>/dev/null |
+    sed "s/4584ed74b7488b4c24083b01/4584ed75b7488b4c24083b01/; s/4584f674b7488b4c24083b01/4584f675b7488b4c24083b01/;" | # P1: [69057,?); P2: [42218,69057);
     xxd -r -p >"${SO_FILE}" 2>/dev/null
   rm -f "${SO_FILE}.tmp"
 
 elif [ "${1}" = "late" ]; then
   echo "Installing addon nvmesystem - ${1}"
+  #mkdir -p "/tmpRoot/usr/rr/addons/"
+  #cp -pf "${0}" "/tmpRoot/usr/rr/addons/"
 
   # disk/shared_disk_info_enum.c::84 Failed to allocate list in SharedDiskInfoEnum, errno=0x900.
   SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
   [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
-
   cp -pf "${SO_FILE}" "${SO_FILE}.tmp"
-  if echo "${PLATFORMS}" | grep -qw "${PLATFORM}"; then
-      xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
-      sed "s/0f95c00fb6c0488b94240810/0f94c00fb6c0488b94240810/; s/8944240c8b44240809e84409/8944240c8b44240890904409/" |
-      xxd -r -p >"${SO_FILE}" 2>/dev/null
-  else    
-    # Activate nvmevolume for Non-DT flatforms
-      xxd -c $(xxd -p "${SO_FILE}.tmp" | wc -c) -p "${SO_FILE}.tmp" | 
-      sed "s/803e00b801000000752.488b/803e00b8010000009090488b/" | 
-      xxd -r -p > "${SO_FILE}"
-  fi  
-  rm -f "${SO_FILE}.tmp"  
+  xxd -c "$(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c)" -p "${SO_FILE}.tmp" 2>/dev/null |
+    sed "s/0f95c00fb6c0488b94240810/0f94c00fb6c0488b94240810/; s/8944240c8b44240809e84409/8944240c8b44240890904409/" | # [69057,?);     (from SA6400 69057)
+    sed "s/0f95c00fb6c0488b94240810/0f94c00fb6c0488b94240810/; s/85e40f884e0100004585ed0f/85e49090909090904585ed0f/" | # [42962,69057); (from SA6400 42962)
+    sed "s/0f95c00fb6c0488b4c242864/0f94c00fb6c0488b4c242864/; s/85e40f884e0100004585ed0f/85e49090909090904585ed0f/" | # [42962,69057); (from DS920+ 42962)
+    sed "s/0f95c00fb6c04883c408c348/0f94c00fb6c04883c408c348/; s/85e40f88580100004585ed0f/85e49090909090904585ed0f/" | # [42218,42962); (from DS920+ 42218)
+    xxd -r -p >"${SO_FILE}" 2>/dev/null
+  rm -f "${SO_FILE}.tmp"
 
   # Create storage pool page without RAID type.
-  cp -vpf nvmesystem.sh /tmpRoot/usr/sbin/nvmesystem.sh
-  chmod +x /tmpRoot/usr/sbin/nvmesystem.sh
+  cp -vpf /usr/bin/nvmesystem.sh /tmpRoot/usr/bin/nvmesystem.sh
 
-  [ ! -f "/tmpRoot/usr/bin/gzip" ] && cp -vpf /usr/bin/gzip /tmpRoot/usr/bin/gzip  
+  [ ! -f "/tmpRoot/usr/bin/gzip" ] && cp -vpf /usr/bin/gzip /tmpRoot/usr/bin/gzip
 
-  cat > /tmpRoot/usr/lib/systemd/system/nvmesystem.service <<'EOF'
-[Unit]
-Description=mshell addon nvmesystem daemon
-Wants=smpkg-custom-install.service pkgctl-StorageManager.service
-After=smpkg-custom-install.service
-After=storagepanel.service
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/sbin/nvmesystem.sh
-[Install]
-WantedBy=multi-user.target
-EOF
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/nvmesystem.service"
+  {
+    echo "[Unit]"
+    echo "Description=RR addon nvmesystem daemon"
+    echo "Wants=smpkg-custom-install.service pkgctl-StorageManager.service"
+    echo "After=smpkg-custom-install.service"
+    echo "After=storagepanel.service" # storagepanel
+    echo
+    echo "[Service]"
+    echo "Type=oneshot"
+    echo "RemainAfterExit=yes"
+    echo "ExecStart=-/usr/bin/nvmesystem.sh"
+    echo
+    echo "[Install]"
+    echo "WantedBy=multi-user.target"
+  } >"${DEST}"
 
   mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
   ln -vsf /usr/lib/systemd/system/nvmesystem.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/nvmesystem.service
 
-elif [ "${1}" = "rcExit" ]; then
+elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon nvmesystem - ${1}"
-  echo "Modifying /linuxrc.syno.impl nvmesystem - ${1}"
-  sed -i 's/WithInternal=0/WithInternal=1/' /linuxrc.syno.impl
-  cat /linuxrc.syno.impl | grep WithInternal
 
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  [ -f "${SO_FILE}.bak" ] && mv -f "${SO_FILE}.bak" "${SO_FILE}"
+
+  rm -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/nvmesystem.service"
+  rm -f "/tmpRoot/usr/lib/systemd/system/nvmesystem.service"
+
+  # rm -f /tmpRoot/usr/bin/gzip
+  [ ! -f "/tmpRoot/usr/rr/revert.sh" ] && echo '#!/usr/bin/env bash' >/tmpRoot/usr/rr/revert.sh && chmod +x /tmpRoot/usr/rr/revert.sh
+  echo "/usr/bin/nvmesystem.sh -r" >>/tmpRoot/usr/rr/revert.sh
+  echo "rm -f /usr/bin/nvmesystem.sh" >>/tmpRoot/usr/rr/revert.sh
 fi
