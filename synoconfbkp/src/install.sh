b1493594a05021f402ced0865d8bdebab222ba06
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-17 16:46:50 +0900

    Update install.sh

--- synoconfbkp/src/install.sh
+++ synoconfbkp/src/install.sh
@@ -8,37 +8,17 @@
 
 if [ "${1}" = "late" ]; then
   echo "Installing addon synoconfbkp - ${1}"
-  mkdir -p "/tmpRoot/usr/rr/addons/"
-  cp -pf "${0}" "/tmpRoot/usr/rr/addons/"
 
-  cp -vpf /usr/bin/rr-synoconfbkp.sh /tmpRoot/usr/bin/rr-synoconfbkp.sh
+  cp -vpf /usr/bin/synoconfbkp.sh /tmpRoot/usr/bin/synoconfbkp.sh
+  chmod 755 /tmpRoot/usr/bin/synoconfbkp.sh
   
   export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
   ESYNOSCHEDULER_DB="/tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db"
-  if [ ! -f "${ESYNOSCHEDULER_DB}" ] || ! /tmpRoot/bin/sqlite3 "${ESYNOSCHEDULER_DB}" ".tables" | grep -qw "task"; then
-    echo "copy esynoscheduler.db"
-    mkdir -p "$(dirname "${ESYNOSCHEDULER_DB}")"
-    cp -vpf /addons/esynoscheduler.db "${ESYNOSCHEDULER_DB}"
-  fi
   echo "insert synoconfbkp task to esynoscheduler.db"
   /tmpRoot/bin/sqlite3 "${ESYNOSCHEDULER_DB}" <<EOF
 DELETE FROM task WHERE task_name LIKE 'SynoconfbkpBootup';
-INSERT INTO task VALUES('SynoconfbkpBootup', '', 'bootup', '', 1, 0, 0, 0, '', 0, "/usr/bin/rr-synoconfbkp.sh ${2:-7} ${3:-bkp}_bootup", 'script', '{}', '', '', '{}', '{}');
+INSERT INTO task VALUES('SynoconfbkpBootup', '', 'bootup', '', 1, 0, 0, 0, '', 0, "/usr/bin/synoconfbkp.sh ${2:-7} ${3:-bkp}_bootup", 'script', '{}', '', '', '{}', '{}');
 DELETE FROM task WHERE task_name LIKE 'SynoconfbkpShutdown';
-INSERT INTO task VALUES('SynoconfbkpShutdown', '', 'shutdown', '', 1, 0, 0, 0, '', 0, "/usr/bin/rr-synoconfbkp.sh ${2:-7} ${3:-bkp}_shutdown", 'script', '{}', '', '', '{}', '{}');
+INSERT INTO task VALUES('SynoconfbkpShutdown', '', 'shutdown', '', 1, 0, 0, 0, '', 0, "/usr/bin/synoconfbkp.sh ${2:-7} ${3:-bkp}_shutdown", 'script', '{}', '', '', '{}', '{}');
 EOF
-elif [ "${1}" = "uninstall" ]; then
-  echo "Installing addon synoconfbkp - ${1}"
-
-  rm -f "/tmpRoot/usr/bin/rr-synoconfbkp.sh"
-
-  export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
-  ESYNOSCHEDULER_DB="/tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db"
-  if [ -f "${ESYNOSCHEDULER_DB}" ]; then
-    echo "delete synoconfbkp task from esynoscheduler.db"
-    /tmpRoot/bin/sqlite3 "${ESYNOSCHEDULER_DB}" <<EOF
-DELETE FROM task WHERE task_name LIKE 'SynoconfbkpBootup';
-DELETE FROM task WHERE task_name LIKE 'SynoconfbkpShutdown';
-EOF
-  fi
 fi
