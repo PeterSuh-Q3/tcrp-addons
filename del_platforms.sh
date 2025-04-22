4f6fe5bc469b99ccd1de2559720da6446439e5f1
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-22 12:05:05 +0900

    Create del_platforms.sh

---
+++ del_platforms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+baseurl="https://raw.githubusercontent.com/PeterSuh-Q3/tcrp-addons/master"  # Base URL
+
+ls -d */ | grep -v -e "9p" | while IFS= read -r dir; do
+  for baseplatform in `cat delplatforms`
+  do 
+    
+    echo "Removing ${baseplatform} to ${dir}rpext-index.json"
+
+    jsonfile="./${dir}rpext-index.json"
+    model_url="${baseurl}/${dir}recipes/universal.json"
+    jq --arg model "${baseplatform}" --arg url "$model_url" '.releases -= { ($model): $url }' "$jsonfile" > temp.json && mv temp.json "$jsonfile"    
+
+  done
+done
