3d95e2b54e7467d80e09039bb31ec9146b85b6b9
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-26 20:52:26 +0900

    Update install.sh

--- hdddbonjunior/src/install.sh
+++ hdddbonjunior/src/install.sh
@@ -94,73 +94,29 @@ if [ "${1}" = "modules" ]; then
         echo fwrev "${fwrev}" >&2      # debug
         echo size_gb "${size_gb}" >&2      # debug
 
-        # JSON 기본 템플릿 생성 (원본 동일)
-        new_entry=$(jq -n \
-          --arg hdmodel "$hdmodel" \
-          --arg fwrev "$fwrev" \
-          --argjson size_gb "$size_gb" \
-          '{
-            ($hdmodel): {
-              ($fwrev): {
-                size_gb: $size_gb,
-                compatibility_interval: [
-                  {
-                    compatibility: "support",
-                    not_yet_rolling_status: "support",
-                    fw_dsm_update_status_notify: false,
-                    barebone_installable: true,
-                    barebone_installable_v2: "auto",
-                    smart_test_ignore: false,
-                    smart_attr_ignore: false
-                  }
-                ]
-              },
-              default: {
-                size_gb: $size_gb,
-                compatibility_interval: [
-                  {
-                    compatibility: "support",
-                    not_yet_rolling_status: "support",
-                    fw_dsm_update_status_notify: false,
-                    barebone_installable: true,
-                    barebone_installable_v2: "auto",
-                    smart_test_ignore: false,
-                    smart_attr_ignore: false
-                  }
-                ]
-              }
-            }
-          }'
-        )    
-        # 임시 파일 생성 함수
-        update_json() {
-          local tmpfile="/tmp/tmpfile.$$.$RANDOM"
-          touch "$tmpfile"
-          echo "$new_entry" > /tmp/new_entry.json
-          jq -s '.[0] * .[1]' /etc/disk_db.json /tmp/new_entry.json > "$tmpfile" && mv "$tmpfile" /etc/disk_db.json
-        }
-    
         if [ -n "${hdmodel}" ] && [ -n "${fwrev}" ]; then
           if [ $(cat "${dbfile}" | grep "${hdmodel}" | wc -l) -gt 0 ]; then
             echo "${hdmodel} is already exists in ${dbfile}, skip writing to /etc/disk_db.json" >&2  # debug
           else
-            # JSON 파일 업데이트
-            if grep '"'"${hdmodel}"'":' /etc/disk_db.json >/dev/null; then
-              update_json --arg hdmodel "$hdmodel" \
-                          --arg fwrev "$fwrev" \
-                          --argjson new "$new_entry" \
-                '.[$hdmodel] += $new[$hdmodel][$fwrev]'            
-            else
-              update_json --argjson new "$new_entry" \
-                '. += $new'
-            fi
+              if grep '"'"${hdmodel}"'":' /etc/disk_db.json >/dev/null; then
+                 # Replace  "WD40PURX-64GVNY0":{  with  "WD40PURX-64GVNY0":{"80.00A80":{ ... }}},
+                  echo "Insert firmware version:"  # debug
+                  sed -i 's#"'"${hdmodel}"'":{#"'"${hdmodel}"'":{"'"${fwrev}"'":{"size_gb":'"${size_gb}"',"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true,"barebone_installable_v2":"auto","smart_test_ignore":false,"smart_attr_ignore":false}]},#' /etc/disk_db.json
+              else
+                 # Add  "WD40PURX-64GVNY0":{"80.00A80":{ ... }}},"default":{ ... }}}
+                  echo "Append drive and firmware:"  # debug
+                  jsondata='"'"${hdmodel}"'":{"'"${fwrev}"'":{"size_gb":'"${size_gb}"',"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true,"barebone_installable_v2":"auto","smart_test_ignore":false,"smart_attr_ignore":false}]},
+                  "default":{"size_gb":'"${size_gb}"',"compatibility_interval":[{"compatibility":"support","not_yet_rolling_status":"support","fw_dsm_update_status_notify":false,"barebone_installable":true,"barebone_installable_v2":"auto","smart_test_ignore":false,"smart_attr_ignore":false}]}}' && echo $jsondata >> /etc/disk_db.json
+                  echo "," >> /etc/disk_db.json
+              fi                    
           fi
-        fi   
-     fi
+       fi   
+    fi
   }
 
-  echo "{}" > /etc/disk_db.json
+  echo "{" > /etc/disk_db.json
   for d in /sys/block/*; do
+    # $d is /sys/block/sata1 etc
     case "$(basename -- "${d}")" in
       sd*|hd*|sata*|sas*)
         getdriveinfo "$d" "sd"
@@ -170,13 +126,13 @@ if [ "${1}" = "modules" ]; then
       ;;
     esac
   done
+  sed -i '$s/,$/}/' /etc/disk_db.json
   #cat /etc/disk_db.json
 
   diskdata=$(jq . /etc/disk_db.json)
-  #jsonfile=$(jq '.disk_compatbility_info |= .+ '"$diskdata" ${dbfile}) && echo $jsonfile | jq . > ${dbfile}
-  jsonfile=$(jq --argjson diskdata "$diskdata" '.disk_compatbility_info |= (. // {}) * $diskdata' ${dbfile})
+  jsonfile=$(jq '.disk_compatbility_info |= .+ '"$diskdata" ${dbfile}) && echo $jsonfile | jq . > ${dbfile}
   # print last 8 elements
-  #jq '.disk_compatibility_info | to_entries | map(select(.value != null)) | .[-8:]' ${dbfile}
+  #jq '.disk_compatbility_info | to_entries | map(select(.value != null)) | .[-8:]' ${dbfile}
 
   cp -vf ${dbfile} /etc/
 
