4d645ae22e93114948f6d5ef3782e9d914aa1c8a
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-28 00:26:45 +0900

    Add files via upload

---
+++ zlastdummycall/src/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env ash
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+if [ "${1}" = "patches" ]; then
+  echo "Installing addon zlastdummycall - ${1}"
+  ash /linuxrc.syno.impl
+fi
