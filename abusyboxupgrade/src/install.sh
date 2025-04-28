b0dca9e141de90237ad82486a0ef73e121318a6c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-28 04:10:00 +0000

    fix typo on_early

--- abusyboxupgrade/src/install.sh
+++ abusyboxupgrade/src/install.sh
@@ -3,7 +3,7 @@
 ### USUALLY SCEMD is the last process run in init, so when scemd is running we are most
 # probably certain that system has finish init process
 
-if [ "${1}" = "on_early" ]; then
+if [ "${1}" = "early" ]; then
   echo "Installing addon abusyboxupgrade - ${1}"
   echo "extract usr-busybox.tgz to /usr/bin/ "
   tar vxfz usr-busybox.tgz -C /usr/bin/
