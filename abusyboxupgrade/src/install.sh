dd28b5529b47d79849d3ee582e239f65362fc9eb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-28 12:40:22 +0900

    Add files via upload

---
+++ abusyboxupgrade/src/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env ash
+
+### USUALLY SCEMD is the last process run in init, so when scemd is running we are most
+# probably certain that system has finish init process
+
+if [ "${1}" = "on_early" ]; then
+  echo "Installing addon abusyboxupgrade - ${1}"
+  echo "extract usr-busybox.tgz to /usr/bin/ "
+  tar vxfz usr-busybox.tgz -C /usr/bin
+  echo "make syboliclink for new busybox "
+  /usr/bin/busybox --install -s /usr/bin
+fi
