486e32907126254fccff152f1c8e54df0d5b46c2
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-05 23:17:16 +0900

    Add files via upload

--- abusyboxupgrade/src/install.sh
+++ abusyboxupgrade/src/install.sh
@@ -6,6 +6,7 @@
 . /etc.defaults/rc.subr
 
 KERNEL_VCODE=`KernelVersionCode "$(KernelVersion)"`
+PLATFORM="$(uname -a | awk '{print $NF}' | cut -d '_' -f2)"
 
 if [ "${1}" = "early" ]; then
   echo "Installing addon abusyboxupgrade - ${1}"
@@ -13,11 +14,14 @@ if [ "${1}" = "early" ]; then
   tar vxfz usr.tgz -C /
 elif [ "${1}" = "modules" ]; then
   echo "Installing addon abusyboxupgrade - ${1}"
-  if [ "$KERNEL_VCODE" -ge "$(KernelVersionCode "5.10")" ]; then
+  if [ ! -f /lib/modules/btrfs.ko ]; then
+    if [ "$KERNEL_VCODE" -ge "$(KernelVersionCode "5.10")" ]; then
       echo "Installing  xor raid6_pq zstd_compress syno_cache_protection btrfs  modules"
-      tar vxfz btrfs_5.10_ko.tgz -C /
-  elif [ "$KERNEL_VCODE" -ge "$(KernelVersionCode "4.4")" ]; then
-      echo "Installing  xxhash ecryptfs zstd_decompress zstd_compress xor raid6_pq btrfs  modules (not now)"
-      #tar vxfz btrfs_4.4_ko.tgz -C /
-  fi
+      tar vxfz btrfs_${PLATFORM}_ko.tgz -C /
+    elif [ "$KERNEL_VCODE" -ge "$(KernelVersionCode "4.4.302")" ]; then
+      echo "Installing  xxhash ecryptfs zstd_decompress zstd_compress xor raid6_pq btrfs  modules"
+      tar vxfz btrfs_${PLATFORM}_ko.tgz -C /
+      exit 0
+    fi
+  fi  
 fi
