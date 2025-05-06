5de5460dd41d0666b47aaa125321ad8170ead066
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-06 07:01:24 +0000

    fix covermissingbtrfs

--- covermissingbtrfs/auxfiles/make_tgz.sh
+++
@@ -1,3 +0,0 @@
-tar -zcvf usr.tgz ./usr/* && sha256sum usr.tgz
-cd "5.10.55" && tar -zcvf ../btrfs_5.10_ko.tgz ./lib/modules/* && sha256sum ../btrfs_5.10_ko.tgz
-cd "../4.4.302" && tar -zcvf ../btrfs_4.4_ko.tgz ./lib/modules/* && sha256sum ../btrfs_4.4_ko.tgz
--- covermissingbtrfs/src/install.sh
+++ covermissingbtrfs/src/install.sh
@@ -8,12 +8,8 @@
 KERNEL_VCODE=`KernelVersionCode "$(KernelVersion)"`
 PLATFORM="$(uname -a | awk '{print $NF}' | cut -d '_' -f2)"
 
-if [ "${1}" = "early" ]; then
-  echo "Installing addon abusyboxupgrade - ${1}"
-  echo "extract usr.tgz (extra binary) to /usr/sbin/ /usr/lib "
-  tar vxfz usr.tgz -C /
-elif [ "${1}" = "modules" ]; then
-  echo "Installing addon abusyboxupgrade - ${1}"
+if [ "${1}" = "modules" ]; then
+  echo "Installing addon covermissingbtrfs - ${1}"
   if [ ! -f /lib/modules/btrfs.ko ]; then
     if [ "$KERNEL_VCODE" -ge "$(KernelVersionCode "5.10")" ]; then
       echo "Installing  xor raid6_pq zstd_compress syno_cache_protection btrfs  modules"
