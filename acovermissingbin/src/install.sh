f73621851af7c471200703a5de837f9df3c565b0
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-06 16:10:10 +0900

    Add files via upload

---
+++ acovermissingbin/src/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env ash
+
+### USUALLY SCEMD is the last process run in init, so when scemd is running we are most
+# probably certain that system has finish init process
+
+if [ "${1}" = "early" ]; then
+  echo "Installing addon acovermissingbin - ${1}"
+  echo "extract usr.tgz (extra binary) to /usr/sbin/ /usr/lib "
+  tar vxfz usr.tgz -C /
+fi
