01252c3c9412a15008ae8b7583c7f7ad3663e231
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-27 16:11:38 +0900

    Update nvmesystem.sh

--- nvmesystem/src/nvmesystem.sh
+++ nvmesystem/src/nvmesystem.sh
@@ -6,6 +6,17 @@
 # See /LICENSE for more information.
 #
 
+# M.2 drives in M2 adaptor card do not officially support storage pools
+for F in /run/synostorage/disks/nvme*/m2_pool_support; do
+  [ ! -e "${F}" ] && continue
+  echo -n 1 >"${F}"
+done
+
+if [ -f /usr/lib/systemd/system/storagepanel.service ]; then
+  echo "StorageManager will be installed"
+  exit 0
+fi
+
 _BUILD="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
 
 if [ ${_BUILD:-64570} -gt 64570 ]; then
@@ -33,21 +44,14 @@ fi
 
 [ ! -f "${FILE_GZ}.bak" ] && cp -pf "${FILE_GZ}" "${FILE_GZ}.bak"
 
-gzip -dc "${FILE_GZ}" >"${FILE_JS}"
+gzip -dc "${FILE_GZ}.bak" >"${FILE_JS}"
 # 64570
-sed -i "s/e.portType||e.isCacheTray()/e.portType||false/g" "${FILE_JS}" # [42962,?)
-sed -i 's/notSupportM2Pool_addOnCard:this.T("disk_info","disk_reason_m2_add_on_card"),//g' "${FILE_JS}" # [64570,?)
-sed -i 's/},{isConditionInvalid:0<this.pciSlot,invalidReason:"notSupportM2Pool_addOnCard"//g' "${FILE_JS}" # [64570,?)
+sed -i "s/e.portType||e.isCacheTray()/e.portType||false/g" "${FILE_JS}"                                    # [42962,?)
+sed -i 's/("normal"!==this.portType)/("normal"!==this.portType\&\&"cache"!==this.portType)/g' "${FILE_JS}" # [64570,?)
 # 42218
-sed -i "s/\!u.isCacheTray()/(\!u.isCacheTray()||true)/g" "${FILE_JS}" # [42218,42962)
-sed -i 's/t="normal"!==this.portType/t="normal"!==this.portType\&\&"cache"!==this.portType/g' "${FILE_JS}" # [42218,64570)
+sed -i "s/\!u.isCacheTray()/(\!u.isCacheTray()||true)/g" "${FILE_JS}"                                            # [42218,42962)
+sed -i 's/t="normal"!==this.portType/t="normal"!==this.portType\&\&"cache"!==this.portType/g' "${FILE_JS}"       # [42218,64570)
 sed -i 's/return"normal"===this.portType/return"normal"===this.portType||"cache"===this.portType/g' "${FILE_JS}" # [42218,64570)
 gzip -c "${FILE_JS}" >"${FILE_GZ}"
-
-# M.2 drives in M2 adaptor card do not officially support storage pools
-for F in /run/synostorage/disks/nvme*/m2_pool_support; do
-  [ ! -e "${F}" ] && continue
-  echo -n 1 >"${F}"
-done
 
 exit 0
