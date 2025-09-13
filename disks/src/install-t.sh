0efe579286a338a3e765e5423246c841c43ca4cb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-09-13 22:48:16 +0900

    Add install-t.sh for managing addon disks

---
+++ disks/src/install-t.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+if [ "${1}" = "patches" ]; then
+  echo "Installing addon disks - ${1}"
+
+  /usr/bin/disks.sh --create
+
+elif [ "${1}" = "late" ]; then
+  echo "Installing addon disks - ${1}"
+  mkdir -p "/tmpRoot/usr/rr/addons/"
+  cp -pf "${0}" "/tmpRoot/usr/rr/addons/"
+
+  cp -vpf /usr/bin/disks.sh /tmpRoot/usr/bin/disks.sh
+  {
+    echo '# Author: "RROrg"'
+    echo ''
+    echo '# general disks dtb rules'
+    echo 'ACTION=="add", SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", ENV{DEVNAME}=="/dev/nvme*|/dev/sas*|/dev/usb*|/dev/sd*|/dev/sata*", PROGRAM=="/usr/bin/disks.sh --update %E{DEVNAME}"'
+  } >"/tmpRoot/usr/lib/udev/rules.d/04-system-disk-dtb.rules"
+
+  if [ "$(/bin/get_key_value "/etc.defaults/synoinfo.conf" "supportportmappingv2")" = "yes" ]; then
+    cp -vpf /usr/bin/dtc /tmpRoot/usr/bin/dtc
+    cp -vpf /etc/model.dtb /tmpRoot/etc/model.dtb
+    cp -vpf /etc/model.dtb /tmpRoot/etc.defaults/model.dtb
+    [ -f "/addons/model.dts" ] && cp -vpf /addons/model.dts /tmpRoot/etc/user_model.dts || rm -rf /tmpRoot/etc/user_model.dts
+  else
+    KVLIST="${KVLIST} usbportcfg esataportcfg internalportcfg"
+
+    cp -vpf /etc/extensionPorts /tmpRoot/etc/extensionPorts
+    cp -vpf /etc/extensionPorts /tmpRoot/etc.defaults/extensionPorts
+  fi
+  KVLIST="${KVLIST} maxdisks supportnvme support_m2_pool" # support_ssd_cache support_write_cache"
+
+  for K in ${KVLIST}; do
+    V="$(/bin/get_key_value "/etc.defaults/synoinfo.conf" "${K}")"
+    for F in "/tmpRoot/etc/synoinfo.conf" "/tmpRoot/etc.defaults/synoinfo.conf"; do
+      /bin/set_key_value "${F}" "${K}" "${V}"
+    done
+    echo "disks addon: ${K}=${V}"
+  done
+
+elif [ "${1}" = "uninstall" ]; then
+  echo "Installing addon beep - ${1}"
+
+  rm -rf "/tmpRoot/usr/bin/disks.sh"
+  rm -rf "/tmpRoot/usr/lib/udev/rules.d/04-system-disk-dtb.rules"
+  rm -rf "/tmpRoot/usr/bin/dtc"
+fi
