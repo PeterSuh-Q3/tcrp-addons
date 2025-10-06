c9804a7acbbe1a40c06119646ca9390291990ebb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-10-06 12:44:30 +0900

    Add files via upload

--- acpid/src/install.sh
+++ acpid/src/install.sh
@@ -1,23 +1,20 @@
-#!/usr/bin/env ash
+#!/usr/bin/env sh
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
-### USUALLY SCEMD is the last process run in init, so when scemd is running we are most
-# probably certain that system has finish init process
+if [ "${1}" = "late" ]; then
+  echo "Installing addon acpid - ${1}"
 
-if [ "${1}" = "modules" ]; then
-  echo "acpid - modules"
-  echo "extract acpid.tgz to /usr/sbin/ "
-  tar xfz /exts/acpid/acpid.tgz -C /
+  tar -zxf ./acpid-7.1.tgz -C /tmpRoot/
 
-  #/usr/sbin/acpid
-elif [ "${1}" = "late" ]; then
-  echo "acpid - late"
-  #/usr/bin/killall acpid
-  echo "Installing daemon for ACPI button"
-  cp -v /usr/sbin/acpid /tmpRoot/usr/sbin/acpid
-  mkdir -p /tmpRoot/etc/acpi/events/
-  cp -v /etc/acpi/events/power /tmpRoot/etc/acpi/events/power
-  cp -v /etc/acpi/power.sh /tmpRoot/etc/acpi/power.sh
-  cp -v /usr/lib/systemd/system/acpid.service /tmpRoot/usr/lib/systemd/system/acpid.service
-  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -vsf /usr/lib/systemd/system/acpid.service /tmpRoot/lib/systemd/system/multi-user.target.wants/acpid.service
+  if [ -f /usr/lib/modules/button.ko ]; then
+    cp -vpf /usr/lib/modules/button.ko /tmpRoot/usr/lib/modules/button.ko
+  else
+    echo "No button.ko found"
+  fi
+
 fi
