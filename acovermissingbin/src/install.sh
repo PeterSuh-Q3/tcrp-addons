f153791e1c0bcb3b0e2d6c42f9b5df031a6fa6b7
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-10-31 21:57:23 +0900

    Modify install.sh to handle different system revisions
    Add conditional extraction for usr.tgz based on system revision

--- acovermissingbin/src/install.sh
+++ acovermissingbin/src/install.sh
@@ -3,8 +3,14 @@
 ### USUALLY SCEMD is the last process run in init, so when scemd is running we are most
 # probably certain that system has finish init process
 
+REVISION="$(uname -a | cut -d ' ' -f4)"
+
 if [ "${1}" = "early" ]; then
   echo "Installing addon acovermissingbin - ${1}"
   echo "extract usr.tgz (extra binary) to /usr/sbin/ /usr/lib "
-  tar vxfz usr.tgz -C /
+  if [ ${REVISION} = "#25556" ]; then
+    gzip -d -c usr.tgz | tar -xvf - -C /
+  else
+    tar vxfz usr.tgz -C /
+  fi  
 fi
