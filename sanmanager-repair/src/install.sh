0cffee880085fb5c9dc3d28842a6628a357716c3
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-11-02 14:20:22 +0900

    Enhance install script with kernel version check
    Added version check for kernel compatibility and adjusted task insertion logic.

--- sanmanager-repair/src/install.sh
+++ sanmanager-repair/src/install.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-PLATFORM="$(uname -a | awk '{print $NF}' | cut -d '_' -f2)"
+KVER_CLEAN=$(uname -r | grep -oP '^\d+\.\d+\.\d+')
+ZPADKVER=$(printf "%01d%03d%03d\n" $(echo "$KVER_CLEAN" | tr '.' ' '))
 
 if [ "${1}" = "late" ]; then
-  if [ "${PLATFORM}" != "bromolow" ]; then
-    echo "sanmanager-repair late"
-    cp -vf sanrepair.sh /tmpRoot/usr/sbin/sanrepair.sh
-    chmod 755 /tmpRoot/usr/sbin/sanrepair.sh
-    
-    if [ -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
-      if [ $(/tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "select count(*) as cnt from task a where task_name = 'sanmanager-repair';") -gt "0" ]; then
-        echo "A sanmanager-repair task already exists at task_name. skipped!!!"
-      else
-        echo "insert sanmanager-repair task"
-        /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "INSERT INTO task VALUES('sanmanager-repair', '', 'bootup', '', 1, 0, 0, 0, '', 0, 'while true; do sleep 10; /usr/sbin/sanrepair.sh; [ -d /config/target/loopback ] && break; done', 'script', '{"running":[17917]}', 1706787067, 0, '{}', '{}');"
-      fi
+
+  if [ "$ZPADKVER" -le 4004059 ]; then
+    echo "(Not Supported) nvmevolume-onthefly - ${1}, It does not work in UEFI boot mode on kernel versions 4.4.59 and earlier." 
+    exit 0
+  fi  
+
+  echo "sanmanager-repair late"
+  cp -vf sanrepair.sh /tmpRoot/usr/sbin/sanrepair.sh
+  chmod 755 /tmpRoot/usr/sbin/sanrepair.sh
+  
+  if [ -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
+    if [ $(/tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "select count(*) as cnt from task a where task_name = 'sanmanager-repair';") -gt "0" ]; then
+      echo "A sanmanager-repair task already exists at task_name. skipped!!!"
     else
-      echo "copy sanmanager-repair task db"
-      mkdir -p /tmpRoot/usr/syno/etc/esynoscheduler
-      cp -f esynoscheduler.db /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db
+      echo "insert sanmanager-repair task"
+      /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "INSERT INTO task VALUES('sanmanager-repair', '', 'bootup', '', 1, 0, 0, 0, '', 0, 'while true; do sleep 10; /usr/sbin/sanrepair.sh; [ -d /config/target/loopback ] && break; done', 'script', '{"running":[17917]}', 1706787067, 0, '{}', '{}');"
     fi
-  fi 
+  else
+    echo "copy sanmanager-repair task db"
+    mkdir -p /tmpRoot/usr/syno/etc/esynoscheduler
+    cp -f esynoscheduler.db /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db
+  fi
+
 fi
