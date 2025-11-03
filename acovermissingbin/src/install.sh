e54fbc2dc06b967e84dae2e3bc09095d0f235c28
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-11-03 23:25:27 +0900

    Enhance install.sh for addon installation process
    Update install script to handle binary extraction and copying.

--- acovermissingbin/src/install.sh
+++ acovermissingbin/src/install.sh
@@ -2,6 +2,7 @@
 
 ### USUALLY SCEMD is the last process run in init, so when scemd is running we are most
 # probably certain that system has finish init process
+set -e
 
 REVISION="$(uname -a | cut -d ' ' -f4)"
 
@@ -10,6 +11,8 @@ if [ "${1}" = "early" ]; then
   echo "extract usr.tgz (extra binary) to /usr/sbin/ /usr/lib "
   if [ ${REVISION} = "#25556" ]; then
     gunzip -c usr.tgz | tar xvf - -C /
+    cp -vf /usr/sbin/6.2.4/blkid /usr/sbin/blkid 
+    cp -vf /usr/lib/6.2.4/* /usr/lib/
   else
     tar vxfz usr.tgz -C /
   fi  
