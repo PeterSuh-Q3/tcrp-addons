b34c482ae4c68575a7959c4dfd1c57ad44eda95a
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-11-04 01:22:48 +0900

    Change extraction source in install.sh for revision #25556
    Updated the installation script to extract from usr6.tgz instead of usr.tgz for revision #25556.

--- acovermissingbin/src/install.sh
+++ acovermissingbin/src/install.sh
@@ -10,7 +10,7 @@ if [ "${1}" = "early" ]; then
   echo "Installing addon acovermissingbin - ${1}"
   echo "extract usr.tgz (extra binary) to /usr/sbin/ /usr/lib "
   if [ ${REVISION} = "#25556" ]; then
-    gunzip -c usr.tgz | tar xvf - -C /
+    gunzip -c usr6.tgz | tar xvf - -C /
     cp -vf /usr/sbin/6.2.4/* /usr/sbin/
     cp -vf /usr/lib/6.2.4/* /usr/lib/
   else
