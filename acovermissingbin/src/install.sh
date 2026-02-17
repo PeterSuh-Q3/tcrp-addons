c1b4714727a3a3faf201f07ad32bce7596c144bc
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-02-17 23:30:21 +0900

    Simplify installation script by removing version checks
    Removed version-specific handling for kernel version 3.

--- acovermissingbin/src/install.sh
+++ acovermissingbin/src/install.sh
@@ -4,17 +4,8 @@
 # probably certain that system has finish init process
 set -e
 
-KVER="$(uname -a | cut -d ' ' -f3)"
-KVER_FIRST="${KVER:0:1}"
-
 if [ "${1}" = "early" ]; then
   echo "Installing addon acovermissingbin - ${1}"
   echo "extract usr.tgz (extra binary) to /usr/sbin/ /usr/lib "
-  if [ "${KVER_FIRST}" = "3" ]; then
-    gunzip -c usr6.tgz | tar xvf - -C /
-    cp -vf /usr/sbin/6.2.4/* /usr/sbin/
-    cp -vf /usr/lib/6.2.4/* /usr/lib/
-  else
-    tar vxfz usr.tgz -C /
-  fi  
+  tar vxfz usr.tgz -C /
 fi
