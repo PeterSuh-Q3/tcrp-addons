2659ff1a11e1e21062d3d33ede7efbf72a1e0701
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-03-05 00:01:34 +0900

    Add firmware file creation for USB 3.0 support
    Add a workaround for xhci-pci kernel panic.

--- acovermissingbin/src/install.sh
+++ acovermissingbin/src/install.sh
@@ -8,4 +8,7 @@ if [ "${1}" = "early" ]; then
   echo "Installing addon acovermissingbin - ${1}"
   echo "extract usr.tgz (extra binary) to /usr/sbin/ /usr/lib "
   tar vxfz usr.tgz -C / >/dev/null 2>&1
+  # Prevent xhci-pci KP when using custom module (NEC USB 3.0 firmware dummy file)
+  mkdir -p /lib/firmware
+  touch /lib/firmware/renesas_usb_fw.mem
 fi
