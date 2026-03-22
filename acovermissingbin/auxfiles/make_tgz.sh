6ce6a14845c3bfa3e195d883421f1696fab46986
PeterSuh-Q3 <dante9000@gmail.com>
2026-03-22 11:53:59 +0900

    Add set_baud BIN to set the speed of /dev/ttyUSB0

--- acovermissingbin/auxfiles/make_tgz.sh
+++ acovermissingbin/auxfiles/make_tgz.sh
@@ -1,2 +1,4 @@
+rm -f ./usr/sbin/.DS_Store
 chmod +x ./usr/sbin/*
-tar -zcvf usr6.tgz ./usr/* && sha256sum usr6.tgz
+tar -zcvf usr.tgz ./usr/* && sha256sum usr.tgz
+#tar -zcvf usr6.tgz ./usr/* && sha256sum usr6.tgz
